99308e5c23ad689d86057d4df5c911ff6c628e91
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-05-20 02:48:14 -0700

    Abed can now jump
    Using the powers of linear algebra, My characters can now jump. The code
    for proper state handling is still quite a mess. I'm going to need to
    refactor eventually.

--- game.lua
+++ game.lua
@@ -11,12 +11,17 @@ function Player.create(sheet_path)
     local g = anim8.newGrid(92, 92, sheet:getWidth(), sheet:getHeight())
 
     setmetatable(plyr, Player)
+    plyr.jumpfunc = function(x) return 0 end
     plyr.sheet = sheet
-    plyr.direction = 'right'
     plyr.pos = {x=300, y=450}
+    plyr.vel = {x=0, y=0}
     plyr.state = 'idle'
-    plyr.dirty = false
+    plyr.x = 0
     plyr.animations = {
+        jump = {
+            right = anim8.newAnimation('once', g('7,2'), 1),
+            left = anim8.newAnimation('once', g('7,1'), 1)
+        },
         walk = {
             right = anim8.newAnimation('loop', g('2-4,2', '3,2'), 0.16),
             left = anim8.newAnimation('loop', g('2-4,1', '3,1'), 0.16)
@@ -29,12 +34,37 @@ function Player.create(sheet_path)
     return plyr
 end
 
-function Player:animation()
-    return self.animations[self.state][self.direction]
+function Player:direction()
+    if self.vel.x < 0 then
+        return 'left'
+    else 
+        return 'right'
+    end
 end
 
-function Player:transition(state)
-    self.state = state
+
+function Player:animation()
+    return self.animations[self.state][self:direction()]
+end
+
+function Player:transition(state, key)
+    if state == 'idle' and key == 'left' and self.vel.x < 0 then
+        self.vel.x = 0
+    elseif state == 'idle' and key == 'right' and self.vel.x > 0 then
+        self.vel.x = 0
+    end
+    
+    if self.state == 'jump' then
+        return
+    elseif state == 'idle' and self.vel.x == 0 then
+        self.state = state
+    elseif state == 'jump' then
+        self.x = 0
+        self.jumpfunc = game.jumpFunction(200, .75)
+        self.state = state
+    elseif state ~= 'idle' then
+        self.state = state
+    end
 end
 
 function Player:draw()
@@ -45,11 +75,38 @@ function Player:reset(direction)
     self.animations['walk'][direction]:gotoFrame(1)
 end
 
+function game.jumpFunction(height, duration)
+    -- (0,0) (duration / 2, height) (0, duration)
+    x1 = 0
+    y1 = 0
+    x2 = duration / 2 
+    y2 = -height
+    x3 = duration
+    y3 = 0
+    denom = (x1 - x2) * (x1 - x3) *(x2 - x3)
+    A = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / denom
+    B = (x3^2 * (y1 - y2) + x2^2 * (y3 - y1) + x1^2 * (y2 - y3)) / denom
+    C = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / denom
+    return function(x) 
+        return A * math.pow(x, 2) + B * x + C
+    end
+end
+
+
 function Player:update(dt)
-    if self.direction == 'left' and self.state == 'walk' then
-        self.pos.x = self.pos.x - 3
-    elseif self.direction == 'right' and self.state == 'walk' then
-        self.pos.x = self.pos.x + 3
+    self.x = self.x + dt
+    self.pos.x = self.pos.x + self.vel.x
+    if self.state == 'jump' then
+        self.pos.y = 450 + math.min(self.jumpfunc(self.x), 0)
+
+        if self.pos.y == 450 then
+            if self.vel.x == 0 then
+                self.state = 'idle'
+            else
+                self.state = 'walk'
+            end
+        end
+
     end
     self:animation():update(dt)
 end
@@ -72,15 +129,21 @@ end
 
 function game.keyreleased(key)
     if (key == "left" or key == "right") then
-        player:transition('idle')
+        player:transition('idle', key)
         player:reset(key)
     end
 end
 
 function game.keypressed(key)
-    if (key == "left" or key == "right") then
-        player.direction = key
+    if key == "left" then
+        player.vel.x = -3
         player:transition('walk')
+    elseif key == "right" then
+        player.vel.x = 3
+        player:transition('walk')
+    elseif key == " " then
+        player.vel.y = -1
+        player:transition('jump')
     end
 end
 
