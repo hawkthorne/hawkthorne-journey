b797e2b51e990b35df411aa15f816636ba0e07a7
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-05-25 00:04:09 -0700

    Actually allow killing and hitting the hippie

--- src/game.lua
+++ src/game.lua
@@ -15,6 +15,7 @@ game.max_x = 6
 game.max_y= 6
 game.step = 300
 game.over = false
+game.drawBoundingBoxes = false
 
 atl.Loader.path = 'maps/'
 atl.Loader.useSpriteBatch = true
@@ -28,6 +29,7 @@ function Enemy.create(sheet_path)
     local g = anim8.newGrid(46, 46, sheet:getWidth(), sheet:getHeight())
 
     setmetatable(enem, Enemy)
+    enem.dead = false
     enem.width = 48
     enem.height = 48
     enem.sheet = sheet
@@ -52,7 +54,19 @@ function Enemy:animation()
     return self.animations[self.state][self.direction]
 end
 
+function Enemy:die()
+    self.state = 'crawl'
+    self.dead = true
+    Collider:setGhost(self.bb)
+end
+
+
+
 function Enemy:update(dt)
+    if self.dead then
+        return
+    end
+
     if self.position.x > player.position.x then
         self.direction = 'left'
     else
@@ -75,11 +89,14 @@ function Enemy:update(dt)
 
     self:animation():update(dt)
     self.bb:moveTo(self.position.x + self.width / 2,
-                   self.position.y + self.height / 2)
+                   self.position.y + self.height / 2 + 10)
 end
 
 function Enemy:draw()
     self:animation():draw(self.sheet, self.position.x, self.position.y) 
+    if game.drawBoundingBoxes then
+        self.bb:draw()
+    end
 end
 
 Player = {}
@@ -91,7 +108,11 @@ function Player.create(sheet_path)
     local g = anim8.newGrid(46, 46, sheet:getWidth(), sheet:getHeight())
 
     setmetatable(plyr, Player)
+    plyr.rebounding = false
+    plyr.invulnerable = false
     plyr.jumping = false
+    plyr.flash = false
+    plyr.timer = 0
     plyr.width = 48
     plyr.height = 48
     plyr.sheet = sheet
@@ -157,9 +178,20 @@ end
 
 function Player:update(dt)
     local step = dt * game.step
+
+    if self.invulnerable then
+        self.timer = self.timer + dt
+        self.flash = not self.flash
+    end
+
+    if self.timer > 2 then
+        Collider:setSolid(self.bb)
+        self.invulnerable = false
+        self.flash = false
+    end
     
     -- taken from sonic physics http://info.sonicretro.org/SPG:Running
-    if love.keyboard.isDown('left') then
+    if love.keyboard.isDown('left') and not player.rebounding then
 
         if self.velocity.x > 0 then
             self.velocity.x = self.velocity.x - (self:deccel() * step)
@@ -170,7 +202,7 @@ function Player:update(dt)
             end
         end
 
-    elseif love.keyboard.isDown('right') then
+    elseif love.keyboard.isDown('right') and not player.rebounding then
 
         if self.velocity.x < 0 then
             self.velocity.x = self.velocity.x + (self:deccel() * step)
@@ -194,7 +226,7 @@ function Player:update(dt)
 
     end
 
-    self.velocity.y = self.velocity.y + game.gravity -- step
+    self.velocity.y = self.velocity.y + game.gravity --* step
     if self.velocity.y > game.max_y then
         self.velocity.y = game.max_y
     end
@@ -205,6 +237,7 @@ function Player:update(dt)
 
     if self.position.y == 300 then
         self.jumping = false
+        self.rebounding = false
     end
 
     -- These calculations shouldn't need to be offset, investigate
@@ -251,7 +284,15 @@ function Player:update(dt)
 end
 
 function Player:draw()
+    if self.flash then
+        return
+    end
+
     self:animation():draw(self.sheet, self.position.x, self.position.y) 
+
+    if game.drawBoundingBoxes then
+        self.bb:draw()
+    end
 end
 
 function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
@@ -261,10 +302,21 @@ function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
         enemy = shape_a.parent
     end
 
+
     -- http://info.sonicretro.org/SPG:Getting_Hit
-    a = player.velocity.x = 2 * math.sign(player.position.x - enemy.position.x)
+    a = math.sign(player.position.x - enemy.position.x)
     if a == 0 then
         a = 1
+    end
+
+    local x1,y1,x2,y2 = enemy.bb:bbox()
+    if player.position.y + player.height <= y2 then
+        enemy:die()
+    else
+        Collider:setGhost(player.bb)
+        player.rebounding = true
+        player.invulnerable = true
+        player.timer = 0
     end
 
     player.velocity.y = -4
@@ -303,7 +355,7 @@ function game.load()
     player.bb = Collider:addRectangle(0,0,18,42)
     player.bb.parent = player
 
-    enemy.bb = Collider:addRectangle(0,0,38,38)
+    enemy.bb = Collider:addRectangle(0,0,30,25)
     enemy.bb.parent = enemy
 
 end
@@ -341,7 +393,7 @@ end
 
 function game.keyreleased(key)
     -- taken from sonic physics http://info.sonicretro.org/SPG:Jumping
-    if key == ' ' then
+    if key == ' ' and not player.rebounding then
         if player.velocity.y < 0 and player.velocity.y < -4 then
             player.velocity.y = -4
         end
@@ -350,7 +402,7 @@ end
 
 function game.keypressed(key)
     -- taken from sonic physics http://info.sonicretro.org/SPG:Jumping
-    if key == ' ' then
+    if key == ' ' and not player.rebounding then
         if player.state ~= 'jump' then
             player.jumping = true
             player.velocity.y = -6.5
