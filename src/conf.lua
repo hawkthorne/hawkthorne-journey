8fff6720caccb754a1810eb7f9f198e06f30cb41
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-05-28 13:21:15 -0700

    Bump release version to v0.0.5

--- src/conf.lua
+++ src/conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-    t.title             = "Journey to the Center of Hawkthorne v0.0.4"
+    t.title             = "Journey to the Center of Hawkthorne v0.0.5"
     t.author            = "Kyle Conroy"
     t.version           = "0.8.0"
 	t.identity          = "hawkthorne"
