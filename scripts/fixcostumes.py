3347c0b7cbbccb3c30d11935337059746655bac4
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-06-17 04:58:09 -0700

    Populate costume with default values

--- scripts/fixcostumes.py
+++ scripts/fixcostumes.py
@@ -17,25 +17,39 @@ def tiles(image):
             yield (48 * x, 48 * y, 48 * (x + 1), 48 * (y + 1))
 
 
+def empty(imagedata):
+    for pixel in imagedata:
+        if not pixel == (255, 255, 255, 0):
+            return False
+    return True
+
+
 for name in ['abed', 'britta', 'jeff', 'annie', 'troy', 'pierce', 'shirley']:
     image = Image.open(COSTUME_BASE.format(name))
-    
+
     for path in costumes(name):
         costume = Image.open(path)
 
         if not costume.size == image.size:
-            print "Resizing canvas\t{}".format(path)
+            print "\t".join([name, "Resizing canvas", path])
             new_costume = Image.new('RGBA', image.size)
             new_costume.paste(costume, (0, 0, costume.size[0], costume.size[1]))
             new_costume.save(path)
 
     for path in costumes(name):
         costume = Image.open(path)
-        
+        log = "{}\tPasting Tile\t({},{})\t{}"
+
         for box in tiles(image):
-            h1 = image.crop(box).histogram()
-            h2 = costume.crop(box).histogram()
-            print h2
+            s1 = image.crop(box)
+            s2 = costume.crop(box)
+
+            if not empty(s1.getdata()) and empty(s2.getdata()):
+                print log.format(name, box[0] / 48, box[1] / 48, path)
+                costume.paste(s1, box)
+
+        costume.save(path)
+
 
 
         
