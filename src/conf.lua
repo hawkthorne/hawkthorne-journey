f67d7af046790d76cc095af0752d8f1fa9c21eba
Jared Parsons <jaredpar@gmail.com>
2012-07-02 22:50:44 -0500

    Added basic fullscreen support

--- src/conf.lua
+++ src/conf.lua
@@ -3,8 +3,9 @@ function love.conf(t)
     t.author            = "Kyle Conroy"
     t.version           = "0.8.0"
 	t.identity          = "hawkthorne"
-    t.screen.width      = 456
-    t.screen.height     = 264
+    t.screen.width      = 0
+    t.screen.height     = 0
+    t.screen.fullscreen = true
 	t.consolne          = false
 	t.modules.physics   = false
     t.modules.joystick  = false
