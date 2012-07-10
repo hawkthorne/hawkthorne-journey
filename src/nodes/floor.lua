40be7764b4f1ffdc33d9901b1c5c7119bfe24136
TJ Kells <tjkells@gmail.com>
2012-07-09 22:34:49 -0500

    add support for regular polygon shapes

--- src/nodes/floor.lua
+++ src/nodes/floor.lua
@@ -6,10 +6,11 @@ function Floor.new(node, collider)
     setmetatable(floor, Floor)
 
     --If the node is a polyline, we need to draw a polygon rather than rectangle
-    if node.polyline then
+    if node.polyline or node.polygon then
+        local polygon = node.polyline or node.polygon
         local vertices = {}
 
-        for k,vertex in ipairs(node.polyline) do
+        for k,vertex in ipairs(polygon) do
             -- Determine whether this is an X or Y coordinate
             if k % 2 == 0 then
                 table.insert(vertices, vertex + node.y)
@@ -20,7 +21,7 @@ function Floor.new(node, collider)
 
         floor.bb = collider:addPolygon( unpack(vertices) )
         -- Stash the polyline on the collider object for future reference
-        floor.bb.polyline = node.polyline
+        floor.bb.polyline = polygon
     else
         floor.bb = collider:addRectangle(node.x, node.y, node.width, node.height)
         floor.bb.polyline = false
@@ -37,7 +38,7 @@ function Floor:collide(player, dt, mtv_x, mtv_y)
     local _, py1, _, py2 = player.bb:bbox()
     local distance = math.abs(player.velocity.y * dt) + 0.10
 
-    if self.bb.polyline and player.velocity.y >= 0 then
+    if self.bb.polyline and player.velocity.y >= 0 and mtv_y < 0 then
         player.velocity.y = 0
         -- Use the MTV to keep players feet on the ground,
         -- fudge the Y a bit to prevent falling into steep angles
