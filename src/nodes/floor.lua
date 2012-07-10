64ae509a15471f7932f91c5a544102c105e385a4
TJ Kells <tjkells@gmail.com>
2012-07-10 11:24:05 -0500

    remove errant block comment

--- src/nodes/floor.lua
+++ src/nodes/floor.lua
@@ -63,19 +63,3 @@ function Floor:collide(player, dt, mtv_x, mtv_y)
 end
 
 return Floor
-
-
--- function Floor:collide(player, dt, mtv_x, mtv_y)
---     local _, wy1, _, wy2  = self.bb:bbox()
---     local _, py1, _, py2 = player.bb:bbox()
---     local distance = math.abs(player.velocity.y * dt) + 0.10
-
-    -- if player.velocity.y >= 0 and math.abs(wy1 - py2) <= distance then
-    --     player.velocity.y = 0
-    --     player.position.y = wy1 - player.height -- fudge factor
-    --     player:moveBoundingBox()
-
-    --     player.jumping = false
-    --     player.rebounding = false
-    -- end
--- end
