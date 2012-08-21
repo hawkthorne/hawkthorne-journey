a4a3f2a353ac84b9bb7fb6b9b15122a1fd475527
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-08-20 23:34:56 -0700

    Britta bot will now tweet every release
    By popular demand, Britta Bot will automatically tweet every release. In
    the future, we'll make her do more things, but for now, release
    announcement is great.

--- scripts/post.py
+++ scripts/post.py
@@ -4,6 +4,7 @@ import requests
 import jinja2
 import json
 import time
+import tweepy
 
 
 class Reddit(object):
@@ -40,8 +41,7 @@ class Reddit(object):
 
         resp.raise_for_status()
 
-        print resp
-        print resp.content
+        return resp
 
 
 url = ("https://api.github.com/repos/kyleconroy/"
@@ -50,11 +50,28 @@ issues_url = "https://api.github.com/repos/kyleconroy/hawkthorne-journey/issues"
 
 title = "[RELEASE] Journey to the Center of Hawkthorne {}"
 
-parser = argparse.ArgumentParser()
-parser.add_argument('base')
-parser.add_argument('head')
-parser.add_argument('-d', '--debug', default=False, action='store_true')
-args = parser.parse_args()
+
+def reddit_url(api_response):
+    """Return the post url from the reddit API response."""
+    # This is because of reddit's insane return values
+    return  api_response['jquery'][-1][3][0]
+
+
+def update_twitter(version, api_response):
+    post_url = reddit_url(api_response)
+
+    consumer_key = "wCIocGQX6rGhkwXGDAIeiw"
+    consumer_secret = os.environ['BRITTA_BOT_CONSUMER_SECRET']
+    access_token = "769023462-ehzty7lqwnJdkcO72iGFzBWymGOy5bGKD90VyB8"
+    access_token_secret = os.environ['BRITTA_BOT_ACCESS_SECRET']
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    tweet = "Journey to Center of Hawkthorne {} {}".format(version, post_url)
+    api.update_status(tweet)
+
 
 def post_content(base, head):
 
@@ -71,16 +88,29 @@ def post_content(base, head):
     bugs = requests.get(issues_url, params={'labels': 'bug'}).json
     features = requests.get(issues_url, params={'labels': 'enhancement'}).json
 
-    return template.render(commits=diff['commits'], version=args.head,
+    return template.render(commits=diff['commits'], version=head,
                            bugs=bugs, features=features)
 
 
-if args.debug:
-    print post_content(args.base, args.head)
-    exit(0)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('base')
+    parser.add_argument('head')
+    parser.add_argument('-d', '--debug', default=False, action='store_true')
+    args = parser.parse_args()
 
-r = Reddit(os.environ['BRITTA_BOT_USER'])
+    if args.debug:
+        print post_content(args.base, args.head)
+        return
 
-r.submit('hawkthorne', title.format(args.head),
-    text=post_content(args.base, args.head),
-    auth=(os.environ['BRITTA_BOT_USER'], os.environ['BRITTA_BOT_PASS']))
+    r = Reddit(os.environ['BRITTA_BOT_USER'])
+
+    resp = r.submit('hawkthorne', title.format(args.head),
+        text=post_content(args.base, args.head),
+        auth=(os.environ['BRITTA_BOT_USER'], os.environ['BRITTA_BOT_PASS']))
+
+    update_twitter(args.head, resp.json)
+
+
+if __name__ == "__main__":
+    main()
