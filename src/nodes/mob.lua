a2e3ae15ca6e077db2646897bdd7118044d04449
Kevin Tuttle <github@kevintuttle.com>
2012-08-31 21:17:33 -0400

    Abstracted mob data to a single table

--- src/nodes/mob.lua
+++ src/nodes/mob.lua
@@ -23,17 +23,32 @@ local menuResponses = {
         },
 }
 
+-- Table format: ID, full name, common name, sprite file (under /images/), sheet format type, instance count
+local mobtable = {
+    ['rich'] = {1, 'Rich Stephenson', 'Dr. Rich', 'rich.png', 1, 0},
+    ['cackowski'] = {2, 'Craig Cackowski', 'Officer Cackowski', 'cackowski.png', 1, 0},
+    ['fatneil'] = {3, 'Neil', 'Fat Neil', 'fatneil.png', 0, 0},
+    ['vicedean'] = {4, 'Robert Laybourne', 'Vice Dean Laybourne', 'vicedean.png', 0, 0},
+    ['vicedean_ghost'] = {5, 'Robert Laybourne', 'Vice Dean Laybourne', 'vicedean_ghost.png', 0, 0},
+    ['chang'] = {6, 'Ben Chang', 'Chang', 'chang.png', 0, 0},
+    ['anniekim'] = {7, 'Annie Kim', 'Annie Kim', 'annie_kim.png', 0, 0},
+    ['buddy'] = {8, 'Buddy Austin', 'Buddy', 'buddy.png', 2, 0},
+    ['luisguzman'] = {9, 'Luis Guzmán', 'Luis Guzmán', 'luis_guzman.png', 2, 0},
+    ['gilbert'] = {10, 'Gilbert Lawson', 'Gilbert', 'gilbert.png', 1, 0},
+    ['garrett'] = {11, 'Garrett Lambert', 'Garrett', 'garrett.png', 1, 0},
+    ['spreck'] = {12, 'Steven Spreck', 'Dean Spreck', 'spreck.png', 0, 0}
+}
 
 function Mob.new(node, collider)
 
     local mob = {}
-    local mobImage = love.graphics.newImage(node.properties.sprites)
+    local mobImage = love.graphics.newImage('images/' .. mobtable[node.properties.id][4])
 
     setmetatable(mob, Mob)
 
     mob.image = mobImage
 
-    if (node.properties.sheetformat == 1) then
+    if (mobtable[node.properties.id][5] == 1) then
         local g = anim8.newGrid(36, 48, mobImage:getWidth(), mobImage:getHeight())
         mob.animations = {
             walking = {
@@ -47,6 +62,22 @@ function Mob.new(node, collider)
             talking = {
                 right = anim8.newAnimation('loop', g('1,2', '3,2'), .8, {[2]=.3}),
                 left = anim8.newAnimation('loop', g('1,1', '3,1'), .8, {[2]=.3}),
+            },
+        }
+    elseif (mobtable[node.properties.id][5] == 2) then
+        local g = anim8.newGrid(48, 48, mobImage:getWidth(), mobImage:getHeight())
+        mob.animations = {
+            walking = {
+                right = anim8.newAnimation('loop', g('2-4,1'), .18),
+                left = anim8.newAnimation('loop', g('2-4,2'), .18),
+            },
+            standing = {
+                right = anim8.newAnimation('loop', g('1,1', '8,1'), 2, {[2]=.4}),
+                left = anim8.newAnimation('loop', g('1,2', '8,2'), 2, {[2]=.4}),
+            },
+            talking = {
+                right = anim8.newAnimation('loop', g('1,1', '5,1'), .8, {[2]=.3}),
+                left = anim8.newAnimation('loop', g('1,2', '5,2'), .8, {[2]=.3}),
             },
         }
     else
@@ -77,8 +108,8 @@ function Mob.new(node, collider)
     mob.width = node.width
     mob.height = node.height
     mob.position = { x = node.x + 12, y = node.y }
-    mob.maxx = node.x + node.properties.roam
-    mob.minx = node.x - node.properties.roam
+    mob.maxx = node.x + node.properties.roam + math.random(1, 10)*24
+    mob.minx = node.x - node.properties.roam - math.random(1, 10)*24
     mob.menu = Menu.new(menuDefinition, menuResponses)
     return mob
 end
