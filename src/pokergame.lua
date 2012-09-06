d06b8c554246d1453f0f2f68bd6e76c03f584258
Adam Nalisnick <theadam@Larry.(none)>
2012-09-05 21:44:41 -0400

    Have the piping for poker done.  Just need hand evaluation

--- src/pokergame.lua
+++ src/pokergame.lua
@@ -53,7 +53,7 @@ function state:init()
     self.options_y = 135
     self.options = {
         { name = 'DRAW', action = 'poker_draw' },
-        { name = 'DEAL', action = 'dealHand' },
+        { name = 'DEAL', action = 'deal_hand' },
         { name = 'BET +', action = function() if self.bet < self.money then self.bet = self.bet + 1 end end },
         { name = 'BET -', action = function() if self.bet > 1 then self.bet = self.bet - 1 end end },
         { name = 'QUIT', action = 'quit', active = true },
@@ -63,6 +63,8 @@ function state:init()
     self.money = 25
 
     self.bet = 2
+
+	self.horizontal_selection = 0
 end
 
 function state:enter(previous, screenshot)
@@ -77,8 +79,8 @@ function state:enter(previous, screenshot)
     
     self.prompt = nil
     
-    self:initTable()
-    self:dealMenu()
+    self:init_table()
+    self:deal_menu()
     
     -- temporary, as this is the only place money is earned currently
     if self.money == 0 then
@@ -108,13 +110,25 @@ function state:keypressed(key, player)
         end
 
         if key == 'return' or key == ' ' then
-			local action = self.options[self.selection + 1].action
-			if(type(action) == 'string') then
-				self[action](self)
-			else --function
-				action()
+			if(self.horizontal_selection == 0) then
+				local action = self.options[self.selection + 1].action
+				if(type(action) == 'string') then
+					self[action](self)
+				else --function
+					action()
+				end
+			else
+				self.player_cards[self.horizontal_selection].raised = not self.player_cards[self.horizontal_selection].raised
 			end
         end
+
+		if self.options[1].active then
+			if key == 'left' or key == 'a' then
+				self.horizontal_selection = (self.horizontal_selection + 1) % 6
+			elseif key == 'right' or key == 'd' then
+				self.horizontal_selection = (self.horizontal_selection - 1) % 6
+			end
+		end
 
         if key == 'up' or key == 'w' then
             repeat
@@ -129,23 +143,34 @@ function state:keypressed(key, player)
     end
 end
 
-function state:gameMenu()
+function state:game_menu()
         -- fix the menu
-        self.selection = 0                  -- deal
-        self.options[ 1 ].active = true     -- hit
-        self.options[ 2 ].active = true     -- stand
-        self.options[ 3 ].active = false    -- deal
-        self.options[ 4 ].active = false    -- bet
-        self.options[ 5 ].active = false    -- bet
+        self.selection = 0                  
+        self.options[ 1 ].active = true     
+        self.options[ 2 ].active = false    
+        self.options[ 3 ].active = false    
+        self.options[ 4 ].active = false
+    	self.horizontal_selection = 0
 end
 
-function state:dealMenu()
+function state:deal_menu()
         -- fix the menu
-        self.selection = 1                   -- deal
-        self.options[ 1 ].active = false    -- hit
-        self.options[ 2 ].active = true     -- deal
-        self.options[ 3 ].active = true     -- bet
-        self.options[ 4 ].active = true     -- bet
+        self.selection = 1                  
+        self.options[ 1 ].active = false    
+        self.options[ 2 ].active = true     
+        self.options[ 3 ].active = true     
+        self.options[ 4 ].active = true
+     	self.horizontal_selection = 0
+end
+
+function state:no_menu()
+        -- fix the menu
+        self.selection = 4                
+        self.options[ 1 ].active = false    
+        self.options[ 2 ].active = false     
+        self.options[ 3 ].active = false    
+        self.options[ 4 ].active = false    
+		self.horizontal_selection = 0
 end
 
 function state:update(dt)
@@ -195,48 +220,64 @@ function state:move_card( card, dt )
     return moved
 end
 
-function state:initTable()
+function state:init_table()
     -- clear everyones cards
     self.dealer_cards = {}
     self.player_cards = {}
     
     -- make a new deck
-    self.deck = newDeck( self.decks_to_use )
-    
-    -- no scores yet
-    self.dealer_hand = nil
-    self.player_hand = nil
-    
-    self.player_done = false
-    self.dealer_done = false
+    self.deck = new_deck( self.decks_to_use )
     
     self.outcome = nil
 end
 
-function state:dealHand()
-    self:initTable()
+function state:deal_hand()
+    self:init_table()
     
-    -- deal first 4 cards
-    self:dealCard( 'player' )
-    self:dealCard( 'dealer' )
-    self:dealCard( 'player' )
-    self:dealCard( 'dealer' )
-    self:dealCard( 'player' )
-    self:dealCard( 'dealer' )
-    self:dealCard( 'player' )
-    self:dealCard( 'dealer' )
-	self:dealCard( 'player' )
-    self:dealCard( 'dealer' )
+	self:no_menu()
+	
+	self.card_complete_callback = function()
+		self.card_complete_callback = nil
+		self:game_menu()
+	end
 
-    self:gameMenu()
-    
-    --check for 21
-    if self:bestScore( self.player_hand ) == 21 then
-        self:stand()
-    end
+    -- deal first 4 cards
+    self:deal_card( 'player' )
+    self:deal_card( 'dealer' )
+    self:deal_card( 'player' )
+    self:deal_card( 'dealer' )
+    self:deal_card( 'player' )
+    self:deal_card( 'dealer' )
+    self:deal_card( 'player' )
+    self:deal_card( 'dealer' )
+	self:deal_card( 'player' )
+    self:deal_card( 'dealer' )
+
 end
 
-function state:dealCard( to )
+function state:poker_draw()
+	self:no_menu()
+	self.card_complete_callback = function()
+		self.card_complete_callback = nil
+		self:deal_menu()
+		for _, card in pairs(self.dealer_cards) do
+			card.face_up = true
+		end
+	end
+	for i = 1, 5 do
+		if self.player_cards[i].raised then
+			self.player_cards[i] = nil
+			self:deal_card('player')
+		end
+		if self.dealer_cards[i].raised then
+			self.dealer_cards[i] = nil
+			self:deal_card('dealer')
+		end
+	end
+	
+end
+
+function state:deal_card( to )
     deal_card = table.remove( self.deck, 1 )
     x = 266
     face_up = true
@@ -248,25 +289,16 @@ function state:dealCard( to )
         tbl = self.dealer_cards
         y = 37
     end
-    table.insert(
-        tbl,
-        {
-            card = deal_card.card,
-            suit = deal_card.suit,
-            x = x - ( self.width_per_card * #tbl ),
-            y = y,
-            move_idx = 0,
-            flip_idx = 0,
-            face_up = face_up
-        }
-    )
-
-    if to == 'dealer' then 
-        self:updateScore( self.dealer_cards )
-    else
-        self:updateScore( self.player_cards )
-    end
-
+	index = get_first_nil(tbl) 
+    tbl[index] = {
+        card = deal_card.card,
+        suit = deal_card.suit,
+        x = x - ( self.width_per_card * (index - 1) ),
+        y = y,
+        move_idx = 0,
+        flip_idx = 0,
+        face_up = face_up
+    }
     -- adjust widths when we've run out of room
     if #tbl * self.width_per_card >= self.max_card_room then
         new_width = self.max_card_room / #tbl
@@ -277,151 +309,10 @@ function state:dealCard( to )
     end
 end
 
-function state:hit()
-    -- throw a card
-    self:dealCard( 'player' )
-    -- bust or still alive?
-    self.card_complete_callback = function()
-        self.card_complete_callback = nil
-        _alive = false
-        if self:bestScore( self.player_hand ) < 21 then
-            _alive = true
-        end
-        if not _alive then
-            self:stand()
-        end
-    end
-end
-
-function state:stand()
-    self.player_done = true
-    
-    -- if not a bust or blackjack, play out the dealers hand
-    if self:bestScore( self.player_hand ) < 21 then
-        while self:bestScore( self.dealer_hand ) < 17 do
-            if not self.dealer_cards[ 2 ].face_up then
-                self.dealer_cards[ 2 ].face_up = true
-                self:updateScore( self.dealer_cards )
-            else
-                self:dealCard( 'dealer' )
-            end
-        end
-    else
-        --flip the dealer over and move on
-        self.dealer_cards[ 2 ].face_up = true
-        self:updateScore( self.dealer_cards )
-    end
-    
-    self.card_complete_callback = function()
-        self.card_complete_callback = nil
-        
-        self.dealer_done = true
-
-        -- determine win, loss, push
-        -- allocate winnings accordingly
-        if self:bestScore( self.player_hand ) == 21 and #self.player_cards == 2
-            and self:bestScore( self.dealer_hand ) ~= 21 then
-            -- player got blackjack!
-            self.money = self.money + ( self.bet * 2 )
-            self.outcome = 'You have Blackjack!\nYou Win!'
-        elseif self:bestScore( self.dealer_hand ) == 21 and #self.dealer_cards == 2
-            and self:bestScore( self.player_hand ) ~= 21 then
-            -- dealer got blackjack!
-            self.money = self.money - self.bet
-            self.outcome = 'Dealer has Blackjack.\nYou Lose.'
-        elseif self:bestScore( self.dealer_hand ) == 22 then
-            -- dealer bust, player wins
-            self.money = self.money + self.bet
-            self.outcome = 'Dealer busted.\nYou Win!'
-        elseif self:bestScore( self.player_hand ) == 22 then
-            -- player pust, player loses
-            self.money = self.money - self.bet
-            self.outcome = 'Busted. You Lose.'
-        elseif self:bestScore( self.dealer_hand ) == self:bestScore( self.player_hand ) then
-            -- push, no winner
-            self.outcome = 'It\'s a push.'
-        elseif self:bestScore( self.dealer_hand ) < self:bestScore( self.player_hand ) then
-            -- player beat dealer, player wins
-            self.money = self.money + self.bet
-            self.outcome = 'You Win!'
-        else
-            -- player lost to dealer, player loses
-            self.money = self.money - self.bet
-            self.outcome = 'You Lost.'
-        end
-        
-        if self.money == 0 then
-            self:gameOver()
-        end
-        
-        if self.money < self.bet then
-            self.bet = self.money
-        end
-
-        self:dealMenu()
-        
-    end
-end
-
-function state:gameOver()
+function state:game_over()
     self.prompt = Dialog.new( 120, 55, "Game Over.", function(result)
         Gamestate.switch(self.previous)
     end )
-end
-
-function state:bestScore( tbl )
-    -- scores should be sorted
-    _best = 22
-    for _,v in pairs( tbl ) do
-        if v <= 21 then
-            _best = v
-        end
-    end
-    return _best
-end
-
-function state:updateScore( tbl )
-    score = {}
-    for i,n in pairs( tbl ) do
-        if n.face_up then
-            if n.card == 1 then -- ace
-                points = { 1, 11 }
-            elseif n.card > 10 then -- face
-                points = { 10 }
-            else
-                points = { n.card }
-            end
-            score = self:addPointsToScore( points, score )
-        end
-    end
-    if tbl == self.dealer_cards then
-        self.dealer_hand = score
-    else
-        self.player_hand = score
-    end
-end
-
-function state:addPointsToScore( points, score )
-    _new = {}
-    if #score == 0 then
-        _new = points
-    else
-        for i = 1, #score, 1 do
-            for j = 1, #points, 1 do
-                table.insert( _new, score[i] + points[j] )
-            end
-        end
-    end
-    table.sort( _new )
-    _no_dup = {}
-    last = nil
-    for x = 1, #_new, 1 do
-        if _new[x] ~= last then
-            table.insert( _no_dup, _new[x] )
-            last = _new[x]
-        end
-    end
-    return _no_dup
 end
 
 function state:draw()
@@ -437,22 +328,25 @@ function state:draw()
     
     if self.dealer_cards then
         for i,n in pairs( self.dealer_cards ) do
-            self:drawCard(
+            self:draw_card(
                 n.card, n.suit,                                                    -- card / suit
                 map( n.flip_idx, 0, self.card_speed, 0, 100 ),                     -- flip
                 map( n.move_idx, 0, self.card_speed, self.dealer_stack_x, n.x ),   -- x
-                map( n.move_idx, 0, self.card_speed, self.dealer_stack_y, n.y )    -- y
+                map( n.move_idx, 0, self.card_speed, self.dealer_stack_y, n.y ),   -- y
+				n.raised and 10 or 0
             )
         end
     end
 
     if self.player_cards then
         for i,n in pairs( self.player_cards ) do
-            self:drawCard(
+            self:draw_card(
                 n.card, n.suit,                                                    -- card / suit
                 map( n.flip_idx, 0, self.card_speed, 0, 100 ),                     -- flip
                 map( n.move_idx, 0, self.card_speed, self.dealer_stack_x, n.x ),   -- x
-                map( n.move_idx, 0, self.card_speed, self.dealer_stack_y, n.y )    -- y
+                map( n.move_idx, 0, self.card_speed, self.dealer_stack_y, n.y ),    -- y
+				n.raised and -10 or 0,
+				i == self.horizontal_selection
             )
         end
     end
@@ -462,7 +356,7 @@ function state:draw()
         local y = self.options_y + ( i * 15 )
         co = 0 -- color offset
         if not n.active then co = 180 end
-        if i == self.selection + 1 then
+        if i == self.selection + 1 and self.horizontal_selection == 0 then
             love.graphics.setColor( 255, 255, 255, 255 )
             love.graphics.draw( self.options_arrow, x - 5, y + 4 )
             co = 255
@@ -502,23 +396,6 @@ function state:draw()
 		end
     end
     
-    if self.dealer_done then
-        _score = self:bestScore( self.dealer_hand )
-        if _score == 22 then
-            love.graphics.print( "BUST", 315, 60, 0, 0.5 )
-        else
-            love.graphics.print( _score, 321, 60, 0, 0.5 )
-        end
-    end
-    if self.player_done then
-        _score = self:bestScore( self.player_hand )
-        if _score == 22 then
-            love.graphics.print( "BUST", 315, 163, 0, 0.5 )
-        else
-            love.graphics.print( _score, 321, 163, 0, 0.5 )
-        end
-    end
-    
     if self.outcome then
         love.graphics.print( self.outcome, 200, 112, 0, 0.5 )
     end
@@ -534,7 +411,7 @@ function state:draw()
     love.graphics.setColor( 255, 255, 255, 255 )
 end
 
-function state:drawCard( card, suit, flip, x, y )
+function state:draw_card( card, suit, flip, x, y, offset, overlay )
     -- flip is a number from 0 to 100, where 0 is completely face down, and 100 is completely face up
     local w = self.card_width   -- card width
     local h = self.card_height  -- card height
@@ -548,11 +425,14 @@ function state:drawCard( card, suit, flip, x, y )
         _card = love.graphics.newQuad( 0, h * 4, w, h, self.cardSprite:getWidth(), self.cardSprite:getHeight() )
     end
     darkness = map( flip, 50, limit, 100, 255 )
+	if(overlay) then
+		darkness = 150
+	end
     love.graphics.setColor( darkness, darkness, darkness )
     love.graphics.drawq(
         self.cardSprite, _card,                             -- image, quad
         x + map( flip, 50, limit, w / 2, 0 ),               -- offset for flip
-        map( flip, 50, limit, y - ( ( sh - h ) / 2 ), y ),  -- height offset
+        map( flip, 50, limit, y - ( ( sh - h ) / 2 ), y ) + offset,  -- height offset
         0,                                                  -- no rotation
         map( flip, 50, limit, 0, 1 ),                       -- scale width for flip
         map( flip, 50, limit , 1 + st, 1 )                  -- scale height for flip
@@ -565,7 +445,7 @@ function map( x, in_min, in_max, out_min, out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 end
 
-function newDeck(_decks)
+function new_deck(_decks)
     if _decks == nil then _decks = 1 end
     deck = {}
     for _deck = 1,_decks,1 do
@@ -596,7 +476,7 @@ function shuffle( deck, n )
     end
 end
 
-function getChipCounts( amount )
+function get_chip_counts( amount )
     _c = { 0, 0, 0, 0, 0 } -- chip stacks
     _min = { 0, 5, 15, 15, 15 } -- min stacks per denomination
     _amt = { 100, 25, 10, 5, 1 } -- value of each denomination
@@ -621,6 +501,15 @@ function getChipCounts( amount )
         amount = amount - ( math.floor( amount / 5 ) * 5 )
     _c[5] = _c[5] + math.floor( amount / 1 )
     return _c
+end
+
+function get_first_nil(t)
+	for i = 1, #t do
+		if(t[i] == nil) then
+			return i
+		end
+	end
+	return #t + 1
 end
 
 return state
