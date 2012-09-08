762337214ea1f5a06e44a090281328469c7d0633
Jordan Hoff <jhoff@linkedin.com>
2012-09-08 17:45:44 -0500

    Bug fix so you can stand on a wall

--- src/nodes/wall.lua
+++ src/nodes/wall.lua
@@ -6,6 +6,7 @@ function Wall.new(node, collider)
     setmetatable(wall, Wall)
     wall.bb = collider:addRectangle(node.x, node.y, node.width, node.height)
     wall.bb.node = wall
+    wall.node = node
     collider:setPassive(wall.bb)
 
     return wall
@@ -17,9 +18,15 @@ function Wall:collide(player, dt, mtv_x, mtv_y)
         player.position.x = player.position.x + mtv_x
     end        
 
-    if mtv_y ~= 0 then
+    if mtv_y > 0 then
         player.velocity.y = 0
         player.position.y = player.position.y + mtv_y
+    end
+    
+    if mtv_y < 0 then
+        player.velocity.y = 0
+        player.position.y = self.node.y - player.height
+        player.jumping = false
     end
 end
 
