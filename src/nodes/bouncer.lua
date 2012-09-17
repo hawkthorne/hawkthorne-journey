be5b710c828b96464d82d878a2823c8f1dbdf638
Jordan Hoff <jhoff@linkedin.com>
2012-09-16 21:31:36 -0500

    New black-cavern tileset ( ADD_attack ) Renamed image filename Added double bounce capabilities to bouncer

--- src/nodes/bouncer.lua
+++ src/nodes/bouncer.lua
@@ -7,15 +7,35 @@ function Bouncer.new(node, collider)
     local bouncer = {}
     setmetatable(bouncer, Bouncer)
     bouncer.bb = collider:addRectangle(node.x, node.y, node.width, node.height)
+    bouncer.node = node
     bouncer.bb.node = bouncer
+    bouncer.bval = node.properties.bval and -(tonumber(node.properties.bval)) or -1000
+    bouncer.dbval = node.properties.dbval and -(tonumber(node.properties.dbval)) or -1500
     collider:setPassive(bouncer.bb)
 
     return bouncer
 end
 
-function Bouncer:collide(player)
-    sound.playSfx('jump')
-    player.velocity.y = -1000
+function Bouncer:collide(player, dt, mtv_x, mtv_y)
+    if player.position.y + player.height > self.node.y + self.node.height then
+        sound.playSfx('jump')
+        if self.double_bounce then
+            player.velocity.y = self.dbval
+        else
+            player.velocity.y = self.bval
+        end
+    end
+end
+
+function Bouncer:keypressed(key)
+    if key == ' ' then
+        self.double_bounce = true
+    end
+end
+
+function Bouncer:collide_end()
+    self.bounced = false
+    self.double_bounce = false
 end
 
 return Bouncer
