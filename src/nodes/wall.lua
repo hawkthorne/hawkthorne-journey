9bc2267e7f55eb6c3ef908644a06535b54830dc9
Jordan Hoff <jhoff@linkedin.com>
2012-10-01 17:36:27 -0500

    Fix wall duck issue / refactor wall collision code Fixed minor sprite issues with abed castle Fixed minor wall node placement in abed castle Fixed web jump through overhang issue in black caverns ( #298 )

--- src/nodes/wall.lua
+++ src/nodes/wall.lua
@@ -14,29 +14,34 @@ function Wall.new(node, collider)
 end
 
 function Wall:collide(player, dt, mtv_x, mtv_y)
-    player.wall_collision = true
-    if player.state == player.crouch_state and mtv_y < player.bbox_height / 2 then
+    local _, wy1, _, wy2 = self.bb:bbox()
+
+    -- if player is crouching ( sliding or not ) and the bottom of the wall is higher than the crouch height, allow it.
+    if player.state == player.crouch_state and wy2 < player.position.y + player.bbox_height / 2 then
+        player.wall_duck = true
         return
     end
 
     if mtv_x ~= 0 then
+        -- horizontal block
         player.velocity.x = 0
         player.position.x = player.position.x + mtv_x
     end
-    
-    if mtv_y > player.bbox_height / 2 - 5 then
-        --player standing up from crouch
-        player.state = player.crouch_state
-        player.position.x = player.position.x + ( 5 * ( player.direction == 'right' and 1 or -1 ) )
-        return
-    end
 
-    if mtv_y > 0 and mtv_y < player.bbox_height / 2 - 5 then
-        player.velocity.y = 0
-        player.position.y = player.position.y + mtv_y
+    if mtv_y > 0 then
+        if player.wall_duck then
+            -- player standing up from crouch
+            player.state = player.crouch_state
+            player.position.x = player.position.x + ( 5 * ( player.direction == 'right' and 1 or -1 ) )
+        else
+            -- bouncing off bottom
+            player.velocity.y = 0
+            player.position.y = player.position.y + mtv_y
+        end
     end
     
     if mtv_y < 0 then
+        -- standing on top
         player.velocity.y = 0
         player.position.y = self.node.y - player.height
         player.jumping = false
@@ -44,7 +49,7 @@ function Wall:collide(player, dt, mtv_x, mtv_y)
 end
 
 function Wall:collide_end(player,dt)
-    player.wall_collision = false
+    player.wall_duck = false
 end
 
 return Wall
