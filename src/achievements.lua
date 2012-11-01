36a01015a707d6fad6d7a2ff4aa66b5005ae4c21
Philip Horger <campadrenalin@gmail.com>
2012-10-31 23:14:05 -0700

    #221 Basic achievements code. Not hooked in yet.

---
+++ src/achievements.lua
@@ -0,0 +1,95 @@
+local AchievementTracker = {}
+AchievementTracker.__index = AchievementTracker
+
+trophies = {
+    'adorable' = {
+        'headline' = "Aww, we're adorable",
+        'description' = "Start a new game",
+        'icon' = nil
+    },
+    "oh cool I'm alive" = {
+        'headline' = "Oh cool, I'm alive!",
+        'description' = "Die once",
+        'icon' = nil
+    },
+}
+
+---
+-- Create a new tracker for achievements.
+-- @return tracker
+function AchievementTracker.new()
+    local tracker = {}
+    setmetatable(tracker, AchievementTracker)
+
+    tracker.counters = {}
+    return tracker
+end
+
+---
+-- Return current count for a tracked label.
+-- @param label
+-- @return count
+function AchievementTracker:getCount(label)
+    if self.counters[label] == nil then
+        self:setCount(label, 0)
+        return 0
+    end
+    return self.counters[label]
+end
+
+---
+-- Set current count for a tracked label.
+-- @param label
+-- @param count
+-- @return nil
+function AchievementTracker:setCount(label, count)
+    self.counters[label] = count
+end
+
+---
+-- Return a list of tracked labels.
+-- @return labels
+function AchievementTracker:getTrackedLabels()
+    local labels = {}
+    for l, _ in ipairs(tracker.counters) do table.insert(labels, l) end
+    return labels
+end
+
+---
+-- Accomplish some tracked task, with optional delta
+-- @param label
+-- @param delta (optional)
+-- @return nil
+function AchievementTracker:achieve(label, delta)
+    delta = delta or 1
+    self:setCount(label, self:getCount(label) + delta)
+    self:onAchieve(label)
+end
+
+---
+-- Code to display achievements as they happen.
+-- @param label
+-- @return nil
+function AchievementTracker:display(label)
+    local info = trophies[label]
+    print(info.headline .. '\n\n\t' .. info.description)
+end
+
+---
+-- Messy logic for individual achievements
+-- @param label
+-- @return nil
+function AchievementTracker:onAchieve(label)
+    count = self:getCount(label)
+    if label == 'start game' then
+        if count == 1 then
+            return self:achieve('adorable')
+        end
+    elseif label == 'die' then
+        if count == 1 then
+            return self:achieve("oh cool I'm alive")
+        end
+    end
+end
+
+return AchievementTracker
