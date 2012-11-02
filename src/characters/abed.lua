87f2ed4ef02ca9b0808cb1fad76850846b10d323
NimbusBP1729 <patjz2@yahoo.com>
2012-11-02 01:43:33 -0400

    added actions for abed

--- src/characters/abed.lua
+++ src/characters/abed.lua
@@ -63,6 +63,10 @@ function plyr.new(sheet)
             right = anim8.newAnimation('loop', g('1-3,9', '2,9'), 0.16),
             left = anim8.newAnimation('loop', g('1-3,10', '2,10'), 0.16),
         },
+        holdjump = { 
+            right = anim8.newAnimation('once', g(1,11), 1),
+            left = anim8.newAnimation('once', g(1,12), 1),
+        },
         crouch = {
             right = anim8.newAnimation('once', g('9,4'), 1),
             left = anim8.newAnimation('once', g('9,3'), 1)
@@ -90,6 +94,26 @@ function plyr.new(sheet)
         attackwalk = {
             left = anim8.newAnimation('loop', g('1,8','5,8','3,8','5,8'), 0.16),
             right = anim8.newAnimation('loop', g('1,7','5,7','3,7','5,7'), 0.16),
+        },
+        wieldwalk = { --state for walking while holding a weapon
+            left = anim8.newAnimation('loop', g('4,8','5,8','6,8','5,8'), 0.16),
+            right = anim8.newAnimation('loop', g('4,7','5,7','6,7','5,7'), 0.16),
+        },
+        wieldidle = { --state for standing while holding a weapon
+            left = anim8.newAnimation('once', g(2,6), 1),
+            right = anim8.newAnimation('once', g(2,5), 1),
+        },
+        wieldjump = { --state for jumping while holding a weapon
+            left = anim8.newAnimation('once', g('7,1'), 1),
+            right = anim8.newAnimation('once', g('7,2'), 1),
+        },
+        wieldaction = { --state for swinging a weapon
+            left = anim8.newAnimation('once', g('6,8','9,8','3,8','6,8'), 0.09),
+            right = anim8.newAnimation('once', g('6,7','9,7','3,7','6,7'), 0.09),
+        },
+        wieldaction2 = { --another state for swinging a weapon
+            left = anim8.newAnimation('once', g('6,8','4,7','3,8','6,8'), 0.09),
+            right = anim8.newAnimation('once', g('6,7','4,8','3,7','6,7'), 0.09),
         },
         jump = {
             right = anim8.newAnimation('once', g('7,2'), 1),
