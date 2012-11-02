4c40234abb17a88a9adf7adf05a9b08a11cf9cda
NimbusBP1729 <patjz2@yahoo.com>
2012-11-02 00:23:39 -0400

    added inheritance to utils file

--- src/utils.lua
+++ src/utils.lua
@@ -140,6 +140,21 @@ function table.contains(t, value)
     return false
 end
 
+--child inherits the members of parent if child doesn't
+-- have a member by the same name
+function inherits(child,parent)
+    for k,v in pairs(parent) do
+        if not child[k] then
+            child[k] = v
+        end
+    end
+    for k,v in pairs(parent["__index"]) do
+        if not child[k] then
+            child[k] = v
+        end
+    end
+    return child
+end
 ------------------------------------------------------------
 --   DRAWING UTILITIES
 ------------------------------------------------------------
