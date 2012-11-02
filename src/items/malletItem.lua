6d08a18cd2afb9d4ddadd0836428ad496375a77f
NimbusBP1729 <patjz2@yahoo.com>
2012-11-02 00:30:06 -0400

    added preliminary weapons

---
+++ src/items/malletItem.lua
@@ -0,0 +1,34 @@
+-----------------------------------------------
+-- malletItem.lua
+-- The code for the mallet, when it in the players inventory.
+-- Created by HazardousPeach
+-----------------------------------------------
+local Global = require 'global'
+local Item = require 'items/item'
+local MalletItem = {}
+MalletItem.__index = MalletItem
+MalletItem.isMalletItem = true
+
+local MalletItemImage = love.graphics.newImage('images/mallet_item.png')
+local Mallet = require 'nodes/mallet'
+
+local GS = require 'vendor/gamestate'
+
+---
+-- Creates a new Mallet item object
+-- @return the Mallet item object created
+function MalletItem.new()
+   local malletItem = {}
+   setmetatable(malletItem, MalletItem)
+   malletItem = Global.inherits(malletItem,Item)
+   malletItem.image = MalletItemImage
+   malletItem.type = 'Weapon'
+   malletItem.quantity = 1
+   malletItem.isHolding = false
+   malletItem.nodeType = "mallet"
+   malletItem.parentNode = Mallet
+
+   return malletItem
+end
+
+return MalletItem
---
+++ src/items/swordItem.lua
@@ -0,0 +1,34 @@
+-----------------------------------------------
+-- swordItem.lua
+-- The code for the sword, when it in the players inventory.
+-- Created by HazardousPeach
+-----------------------------------------------
+local Global = require 'global'
+local Item = require 'items/item'
+local SwordItem = {}
+SwordItem.__index = SwordItem
+SwordItem.isSwordItem = true
+
+local SwordItemImage = love.graphics.newImage('images/sword_item.png')
+local Sword = require 'nodes/sword'
+
+local GS = require 'vendor/gamestate'
+
+---
+-- Creates a new Sword item object
+-- @return the Sword item object created
+function SwordItem.new()
+   local swordItem = {}
+   setmetatable(swordItem, SwordItem)
+   swordItem = Global.inherits(swordItem,Item)
+   swordItem.image = SwordItemImage
+   swordItem.type = 'Weapon'
+   swordItem.quantity = 1
+   swordItem.isHolding = false
+   swordItem.nodeType = "sword"
+   swordItem.parentNode = Sword
+
+   return swordItem
+end
+
+return SwordItem
