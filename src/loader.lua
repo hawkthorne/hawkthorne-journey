76319b6ad149d455abbc659b88b5a30e8d0c1b14
Philip Horger <campadrenalin@gmail.com>
2012-11-06 15:25:15 -0800

    More cleanup, probably deserves more comments

--- src/loader.lua
+++ src/loader.lua
@@ -7,7 +7,7 @@ local state = Gamestate.new()
 local nextState = 'menu'
 local nextPlayer = nil
 
-local preload_queue = {}
+local queue = {}
 local current_preload = {}
 current_preload.order = 1
 current_preload.position = 1
@@ -28,10 +28,11 @@ function state:update(dt)
         return
     end
 
-    self:preload_iterate()
-    local current = self:preload_get_current()
+    self:iterate()
+    local current = self:get_current()
     if current[1] and current[2] and current[3] then
         self.current = self.current + 1
+        --callback(       key,      value)
         current[3](current[1], current[2])
     else
         self.finished = true
@@ -51,16 +52,18 @@ end
 --- Higher order == loaded later
 function state:preload(input_table, callback, order)
     if not order then order = 1 end
-
-    if not preload_queue[order] then
-        preload_queue[order] = {}
+    if not queue[order] then
+        queue[order] = {}
     end
 
     local preload_item = {}
     preload_item.data = input_table
     preload_item.callback = callback
 
-    table.insert(preload_queue[order], preload_item)
+    -- Insert preload data in queue at proper order
+    table.insert(queue[order], preload_item)
+
+    -- Increase total asset count
     local asset_increase = 0
     for _, _ in pairs(input_table) do
         asset_increase = asset_increase + 1
@@ -68,9 +71,9 @@ function state:preload(input_table, callback, order)
     self.total_assets = self.total_assets + asset_increase
 end
 
-function state:preload_iterate()
+function state:iterate()
     if current_preload.current_key == 0 then
-        self:preload_populate_current_table()
+        self:populate_current_table()
         return
     end
     current_preload.current_key = current_preload.current_key + 1
@@ -78,44 +81,44 @@ function state:preload_iterate()
         return
     end
     current_preload.position = current_preload.position + 1
-    if self:preload_get_current_table() then
-        self:preload_populate_current_table()
+    if self:get_current_table() then
+        self:populate_current_table()
         return
     end
     current_preload.order = current_preload.order + 1
     current_preload.position = 1
-    if self:preload_get_current_table() then
-        self:preload_populate_current_table()
+    if self:get_current_table() then
+        self:populate_current_table()
         return
     end
 end
 
-function state:preload_populate_current_table()
+function state:populate_current_table()
     current_preload.current_key = 1
     current_preload.keys = {}
-    for key, _ in pairs(self:preload_get_current_table()) do
+    for key, _ in pairs(self:get_current_table()) do
         table.insert(current_preload.keys, key)
     end
 end
 
-function state:preload_get_current_callback()
-    if not preload_queue[current_preload.order] or not preload_queue[current_preload.order][current_preload.position] then
+function state:get_current_callback()
+    if not queue[current_preload.order] or not queue[current_preload.order][current_preload.position] then
         return nil
     end
-    return preload_queue[current_preload.order][current_preload.position]['callback']
+    return queue[current_preload.order][current_preload.position]['callback']
 end
 
-function state:preload_get_current_table()
-    if not preload_queue[current_preload.order] or not preload_queue[current_preload.order][current_preload.position] then
+function state:get_current_table()
+    if not queue[current_preload.order] or not queue[current_preload.order][current_preload.position] then
         return nil
     end
-    return preload_queue[current_preload.order][current_preload.position]['data']
+    return queue[current_preload.order][current_preload.position]['data']
 end
 
-function state:preload_get_current()
+function state:get_current()
     local current_key = current_preload.keys[current_preload.current_key]
-    local current_table = self:preload_get_current_table()
-    local current_callback = self:preload_get_current_callback()
+    local current_table = self:get_current_table()
+    local current_callback = self:get_current_callback()
 
     local current_value = nil
     if current_table then
