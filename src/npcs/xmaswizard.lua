aa24f92bde1a180b73ef0aba6de2ff92ad9d4ecd
didory123 <didory1356@gmail.com>
2012-12-30 02:45:32 -0800

    winter wonderland level

---
+++ src/npcs/xmaswizard.lua
@@ -0,0 +1,36 @@
+local xmas = {}
+
+xmas.sprite = love.graphics.newImage('images/npc/christmaswizard.png')
+xmas.tickImage = love.graphics.newImage('images/menu/selector.png')
+xmas.menuImage = love.graphics.newImage('images/npc/xmas-wizard-menu.png')
+xmas.walk = false
+xmas.stare = true
+
+xmas.items = {
+    { ['text']='exit' },
+    { ['text']='inventory' },
+    { ['text']='command' },
+    { ['text']='talk', ['option']={
+        { ['text']='i am done with you' },
+        { ['text']='How do I get out of here?' }, 
+        { ['text']='Professor Duncan?' },
+        { ['text']='Who are you?' },
+    }},
+}
+
+xmas.responses = {
+    ["Who are you?"]={
+        "I am a Christmas Wizard!",
+        "And definitely not a psych professor.",
+    },
+    ["How do I get out of here?"]={
+        "You must venture to the Cave of Frozen Memories,",
+        "And there you shall find the exit.",
+    },
+    ["Professor Duncan?"]={
+        "I do not have the slightest idea",
+        "What you're talking about.",
+    },
+}
+
+return xmas
