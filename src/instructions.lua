92d793433e7dee82c13f4c2157ae6750a0a440f8
Nick Scripp <scrippman@gmail.com>
2013-01-11 23:41:43 -0700

    Adds control config to instructions screen

--- src/instructions.lua
+++ src/instructions.lua
@@ -3,24 +3,39 @@ local window = require 'window'
 local camera = require 'camera'
 local sound = require 'vendor/TEsound'
 local fonts = require 'fonts'
-local state = Gamestate.new()
 local controls = require 'controls'
 local VerticalParticles = require "verticalparticles"
+local Menu = require 'menu'
+local state = Gamestate.new()
+local menu = Menu.new({
+    'UP',
+    'DOWN',
+    'LEFT',
+    'RIGHT',
+    'SELECT',
+    'START',
+    'JUMP',
+    'ACTION'})
 
+menu:onSelect(function()
+    controls.enableRemap = true end)
 
 function state:init()
     VerticalParticles.init()
 
+    self.arrow = love.graphics.newImage("images/menu/arrow.png")
     self.background = love.graphics.newImage("images/menu/pause.png")
     self.instructions = {}
 
     -- The X coordinates of the columns
-    self.left_column = 136
-    self.right_column = 245
+    self.left_column = 160
+    self.right_column = 300
     -- The Y coordinate of the top key
     self.top = 93
     -- Vertical spacing between keys
     self.spacing = 20
+
+    self.statusText = 'TEST'
 end
 
 function state:enter(previous)
@@ -30,6 +45,7 @@ function state:enter(previous)
     camera:setPosition(0, 0)
     self.instructions = controls.getButtonmap()
     self.previous = previous
+    self.option = 0
 end
 
 function state:leave()
@@ -37,12 +53,9 @@ function state:leave()
 end
 
 function state:keypressed( button )
-    if button == 'ACTION' then
-        local key = controls.getKey(button)
-        controls.newButton('z', button)
-        return
-    end
-    Gamestate.switch(self.previous)
+    if controls.enableRemap then self:remapKey(button) end
+    if controls.getButton then menu:keypressed(button) end
+    if button == 'START' then Gamestate.switch(self.previous) end
 end
 
 function state:update(dt)
@@ -59,22 +72,33 @@ function state:draw()
     local n = 1
 
     love.graphics.setColor(255, 255, 255)
-    local back = controls.getKey("JUMP") .. ": BACK TO MENU"
+    local back = controls.getKey("START") .. ": BACK TO MENU"
+    local howto = controls.getKey("ACTION") .. " OR " .. controls.getKey("SELECT") .. ": REASSIGN CONTROL"
     love.graphics.print(back, 25, 25)
-
+    love.graphics.print(howto, 25, 55)
+    love.graphics.print(self.statusText, self.left_column, 280)
     love.graphics.setColor( 0, 0, 0, 255 )
 
-    for key, action in pairs(self.instructions) do
-        local y = self.top + self.spacing * (n - 1)
-        -- Draw action
-        love.graphics.print(key, self.left_column, y, 0, 0.8)
-        -- And draw associated key
-        love.graphics.print(action, self.right_column, y, 0, 0.8)
-
-        n = n + 1
+    for i, button in ipairs(menu.options) do
+        local y = self.top + self.spacing * (i - 1)
+        local key = controls.getKey(button)
+        love.graphics.print(button, self.left_column, y, 0, 0.8)
+        love.graphics.print(key, self.right_column, y, 0, 0.8)
     end
-
+    
     love.graphics.setColor( 255, 255, 255, 255 )
+    love.graphics.draw(self.arrow, 135, 87 + self.spacing * menu:selected())
+end
+
+function state:remapKey(key)
+    local button = menu.options[menu:selected() + 1]
+    self.statusText = button .. ": " .. key
+    if controls.newButton(key, button) then
+        assert(controls.getKey(button) == key)
+    else
+        self.statusText = "KEY IS ALREADY IN USE"
+    end
+    controls.enableRemap = false
 end
 
 return state
