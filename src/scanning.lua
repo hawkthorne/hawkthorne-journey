b3dbfe795612d5e44f3851a3107b67ade8555d1c
edisonout <edisonouthawkthorne@gmail.com>
2013-03-17 16:48:36 +0000

    add annie scanning

--- src/scanning.lua
+++ src/scanning.lua
@@ -1,195 +1,90 @@
 local Gamestate = require 'vendor/gamestate'
-local Level = require 'level'
 local window = require 'window'
-local fonts = require 'fonts'
-local background = require 'selectbackground'
-local state = Gamestate.new()
+local camera = require 'camera'
 local sound = require 'vendor/TEsound'
-local Character = require 'character'
-local characters = Character.characters
-local controls = require 'controls'
+local Timer = require 'vendor/timer'
+local anim8 = require 'vendor/anim8'
+local fonts = require 'fonts'
+local state = Gamestate.new()
+local part = require 'verticalparticles'
+local character = require 'character'
 
-local character_selections = {}
-character_selections[1] = {} -- main characters
-character_selections[1][0] = {} -- left
-character_selections[1][1] = {} -- right
-character_selections[1][1][0] = characters['troy']
-character_selections[1][1][1] = characters['shirley']
-character_selections[1][1][2] = characters['pierce']
-character_selections[1][0][0] = characters['jeff']
-character_selections[1][0][1] = characters['britta']
-character_selections[1][0][2] = characters['abed']
-character_selections[1][0][3] = characters['annie']
-
-character_selections[2] = {} -- page 2
-character_selections[2][0] = {} -- left
-character_selections[2][1] = {} -- right
-character_selections[2][1][0] = characters['chang']
-character_selections[2][1][1] = characters['fatneil']
-character_selections[2][1][2] = characters['vicedean']
-character_selections[2][0][0] = characters['dean']
-character_selections[2][0][1] = characters['guzman']
-character_selections[2][0][2] = characters['buddy']
-character_selections[2][0][3] = characters['leonard']
-
-character_selections[3] = {} -- page 3
-character_selections[3][0] = {} -- left
-character_selections[3][1] = {} -- right
-character_selections[3][1][0] = characters['ian']
-character_selections[3][1][1] = characters['rich']
-character_selections[3][1][2] = characters['vicki']
-character_selections[3][0][0] = characters['vaughn']
-character_selections[3][0][1] = characters['kevin']
-
-
-local current_page = 1
-local selections = character_selections[current_page]
+local annieanimation1, annieanimation2, annieanimation3, annieanimation4, annieanimation5, annieanimation6, annieanimation7, annieanimation8 
 
 function state:init()
-    self.side = 0 -- 0 for left, 1 for right
-    self.level = 0 -- 0 through 3 for characters
+  self.image1 = love.graphics.newImage('images/scanning/anniebackground.png')
+  self.image2 = love.graphics.newImage('images/scanning/anniescan.png')
+  self.image3 = love.graphics.newImage('images/scanning/anniesprite.png')
+  self.image4 = love.graphics.newImage('images/scanning/anniename.png')
+  self.image5 = love.graphics.newImage('images/scanning/computer.png')
+  self.image6 = love.graphics.newImage('images/scanning/description.png')
+  self.image7 = love.graphics.newImage('images/scanning/scanningbar.png')
+  self.image8 = love.graphics.newImage('images/scanning/scanningwords.png')
 
-    background.init()
-    self.chartext = ""
-    self.costtext = ""
-end
+  local g = anim8.newGrid(400, 250, self.image1:getWidth(), self.image1:getHeight())
+  local h = anim8.newGrid(121, 172, self.image2:getWidth(), self.image2:getHeight())
+  local i = anim8.newGrid(121, 172, self.image3:getWidth(), self.image3:getHeight())
+  local j = anim8.newGrid(75, 15, self.image4:getWidth(), self.image4:getHeight())
+  local k = anim8.newGrid(75, 19, self.image5:getWidth(), self.image5:getHeight())
+  local l = anim8.newGrid(121, 13, self.image6:getWidth(), self.image6:getHeight())
+  local m = anim8.newGrid(121, 13, self.image7:getWidth(), self.image7:getHeight())
+  local n = anim8.newGrid(121, 13, self.image8:getWidth(), self.image8:getHeight())
 
-function state:enter(previous)
-    fonts.set( 'big' )
-    self.previous = previous
-    self.music = sound.playMusic( "opening" )
-    background.enter()
-    background.setSelected( self.side, self.level )
+  annieanimation1 = anim8.newAnimation('once', g('1, 1'), 0.2)
+  annieanimation2 = anim8.newAnimation('loop', h('1-5, 1' , '1-5, 2' , '1-5, 3', '1-4, 4'), 0.05)
+  annieanimation3 = anim8.newAnimation('loop', i('1-5, 1','1-5, 2','1, 3'), 0.1)
+  annieanimation4 = anim8.newAnimation('loop', j('1, 1-6'), 0.2)
+  annieanimation5 = anim8.newAnimation('loop', k('1, 1-9'), 0.05)
+  annieanimation6 = anim8.newAnimation('loop', l('1, 1-8'), 0.1)
+  annieanimation7 = anim8.newAnimation('loop', m('1, 1-5'), 0.4)
+  annieanimation8 = anim8.newAnimation('loop', n('1, 1-4','1,1-4','1,5'), 0.2)
 
-    self.chartext = "PRESS " .. controls.getKey('JUMP') .. " TO CHOOSE CHARACTER" 
-    self.costtext = "PRESS " .. controls.getKey('ATTACK') .. " TO CHANGE COSTUME"
-end
-
-function state:character()
-    return selections[self.side][self.level]
 end
 
 function state:keypressed( button )
-    if button == "START" then
-      Gamestate.switch(self.previous)
-      return true
-    end
-
-    -- If any input is received while sliding, speed up
-    if background.slideIn or background.slideOut then
-        background.speed = 10
-        return
-    end
-
-    local level = self.level
-    local options = 4
-
-    if button == 'LEFT' or button == 'RIGHT' then
-        self.side = (self.side - 1) % 2
-        sound.playSfx('click')
-    elseif button == 'UP' then
-        level = (self.level - 1) % options
-        sound.playSfx('click')
-    elseif button == 'DOWN' then
-        level = (self.level + 1) % options
-        sound.playSfx('click')
-    end
-
-    if button == 'ATTACK' then
-        if self.level == 3 and self.side == 1 then
-            return
-        else
-            local c = self:character()
-            if c then
-                c.count = (c.count + 1)
-                if c.count == (#c.costumes + 1) then
-                    c.count = 1
-                end
-                c.costume = c.costumes[c.count].sheet
-                sound.playSfx('click')
-            end
-        end
-        return
-    end
-
-    self.level = level
-
-    if ( button == 'JUMP' ) and self.level == 3 and self.side == 1 then
-        current_page = current_page % #character_selections + 1
-        selections = character_selections[current_page]
-        sound.playSfx('confirm')
-    elseif button == 'JUMP' then
-        if self:character() then
-            -- Tell the background to transition out before changing scenes
-            background.slideOut = true
-        end
-        sound.playSfx('confirm')
-    end
-    
-    background.setSelected( self.side, self.level )
+    Timer.clear()
+    Gamestate.switch("select")
 end
 
-function state:leave()
-    fonts.reset()
-end
+-- animation runs for 2 secs
+Timer.add(2, function() Gamestate.switch('select') end)
+
 
 function state:update(dt)
-    -- The background returns 'true' when the slide-out transition is complete
-    if background.update(dt) then
-        -- set the selected character and costume
-        Character:setCharacter( self:character().name )
-        Character:setCostume( self:character().costumes[self:character().count].sheet )
-        Character.changed = true
-        
-        love.graphics.setColor(255, 255, 255, 255)
-        local level = Gamestate.get('overworld')
-        level:reset()
-        Gamestate.switch('flyin')
-    end
+  annieanimation1:update(dt)
+  annieanimation2:update(dt)
+  annieanimation3:update(dt)
+  annieanimation4:update(dt)
+  annieanimation5:update(dt)
+  annieanimation6:update(dt)
+  annieanimation7:update(dt)
+  annieanimation8:update(dt)
 end
+
 
 function state:draw()
-    background.draw()
 
-    -- Only draw the details on the screen when the background is up
-    if not background.slideIn then
-        local name = ""
+  --background colour
+  love.graphics.setColor( 0, 0, 0, 255 )
+  love.graphics.rectangle( 'fill', 0, 0, love.graphics:getWidth(), love.graphics:getHeight() )
+  love.graphics.setColor( 255, 255, 255, 255 )
 
-        if self:character() then
-            name = self:character().costumes[self:character().count].name
-        end
+  --table
+  local width = window.width
+  local height = window.height
+  local xcorner = width/2 - self.image1:getWidth()/2
+  local ycorner = height/2 - self.image1:getHeight()/2
+  
+  -- animations
+  annieanimation1:draw(self.image1, xcorner, ycorner)
+  annieanimation2:draw(self.image2, xcorner + 39, ycorner + 30)
+  annieanimation3:draw(self.image3, xcorner + 240, ycorner + 30)
+  annieanimation4:draw(self.image4, xcorner + 162, ycorner + 35)
+  annieanimation5:draw(self.image5, xcorner + 162, ycorner + 150)
+  annieanimation6:draw(self.image6, xcorner + 39, ycorner + 210)
+  annieanimation7:draw(self.image7, xcorner + 240, ycorner + 210)
+  annieanimation8:draw(self.image8, xcorner + 240, ycorner + 225)
 
-        love.graphics.printf(self.chartext, 0,
-            window.height - 55, window.width, 'center')
-        love.graphics.printf(self.costtext, 0,
-            window.height - 35, window.width, 'center')
-
-        love.graphics.printf(name, 0,
-            23, window.width, 'center')
-
-        local x, y = background.getPosition(1, 3)
-        love.graphics.setColor(255, 255, 255, 200)
-        love.graphics.print("INSUFFICIENT", x, y + 5, 0, 0.5, 0.5, 12, -6)
-        love.graphics.print(  "FRIENDS"   , x, y + 5, 0, 0.5, 0.5, -12, -32)
-        love.graphics.print( current_page .. ' / ' .. #character_selections, x + 60, y + 15, 0, 0.5, 0.5 )
-        love.graphics.setColor(255, 255, 255, 255)
-    end
-
-    for i=0,1,1 do
-        for j=0,3,1 do
-            local character = selections[i][j]
-            local x, y = background.getPosition(i, j)
-            if character then
-                if i == 0 then
-                    love.graphics.drawq( Character:getSheet(character.name, character.costumes[character.count].sheet ), character.mask , x, y, 0, -1, 1 )
-                else
-                    love.graphics.drawq( Character:getSheet(character.name, character.costumes[character.count].sheet ), character.mask , x, y )
-                end
-            end
-        end
-    end
 end
-
-Gamestate.home = state
 
 return state
