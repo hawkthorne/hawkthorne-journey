d9937eaa6950c9b33e6e128aa01ba16f1742933f
didory123 <didory1356@gmail.com>
2013-04-05 23:39:21 -0700

    bats drop coins instead of 1ups now

--- src/nodes/enemies/bat.lua
+++ src/nodes/enemies/bat.lua
@@ -15,7 +15,7 @@ return {
     dyingdelay = 5,
     tokens = 1,
     tokenTypes = { -- p is probability ceiling and this list should be sorted by it, with the last being 1
-        { item = 'life', v = 1, p = 1 },
+        { item = 'coin', v = 1, p = 1 },
     },
     animations = {
         dying = { -- same as dive
