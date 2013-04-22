b17272aa4ea7ca375a25a72c61842b98706aaac8
Brent Merle <ihatespam234@gmail.com>
2013-04-21 22:02:19 -0400

    Added code to determine where the floor is..
    This should be optimized and moved to a more common location.. I'm not
    sure where yet.

--- src/nodes/dropbear.lua
+++ src/nodes/dropbear.lua
@@ -26,7 +26,39 @@ end
 -- Node entrance function
 function DropBear:enter()
     -- Determine the floor's location
-    self.node.floor = gamestate.currentState().map.objectgroups.block.objects[1].y - self.height
+    local x = self.node.x
+    local y = self.node.y + self.height
+
+    -- Set the closestFloor location to be sufficiently large
+    local closestFloor = 1000000
+    local found = false
+
+    -- Iterate over the platforms and blocks to determine the best candidate
+    -- If the platform/block's x + width falls in range of the target x AND
+    -- the platform/block's y is below the target y AND
+    -- the platform/block's y is closer to the target y
+    -- v.x <= x weeds out all platforms/blocks that are way to the right
+    if gamestate.currentState().map.objectgroups.platform then
+        for k,v in pairs(gamestate.currentState().map.objectgroups.platform.objects) do
+            if (v.x <= x and v.x + v.width >= x and v.y > y and v.y < closestFloor) then
+                found = true
+                closestFloor = v.y
+            end
+        end
+    end
+    -- Iterate over the blocks to determine the best candidate
+    if gamestate.currentState().map.objectgroups.block then
+        for k,v in pairs(gamestate.currentState().map.objectgroups.block.objects) do
+            if (v.x <= x and v.x + v.width >= x and v.y > y and v.y < closestFloor) then
+                found = true
+                closestFloor = v.y
+            end
+        end
+    end
+    if not found then
+        print ( "Warning: no floor found for node: " .. self.node.type .. " at (" .. self.node.x .. "," .. self.node.y .. ")" )
+    end
+    self.node.floor = closestFloor - self.height
 end
 
 function DropBear:update(dt, player)
