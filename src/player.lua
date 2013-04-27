73baa1be7e255b2ccaa101abbaaec768f6b40433
Protuhj <ihatespam234@gmail.com>
2013-04-27 03:15:24 -0400

    Deselect a weapon when saving

--- src/player.lua
+++ src/player.lua
@@ -931,17 +931,14 @@ end
 -- Saves necessary player data to the gamesave object
 -- @param gamesave the gamesave object to save to
 function Player:saveData( gamesave )
+    -- If holding a weapon, deselect it before saving
+    if self.currently_held ~= nil and self.currently_held.isWeapon then
+        self.currently_held:deselect()
+    end
     -- Save the inventory
     self.inventory:save( gamesave )
     -- Save our money
     gamesave:set( 'coins', self.money )
-    -- Save off the current_held item since it isn't part of the inventory
-    if self.currently_held ~= nil and self.currently_held.isWeapon then
-        gamesave:set( 'currently_held', json.encode( self.currently_held.item:getSaveData() ) )
-    else
-        -- Reset the saved currently_held data
-        gamesave:set( 'currently_held', nil )
-    end
 end
 
 -- Loads necessary player data from the gamesave object
@@ -952,20 +949,6 @@ function Player:loadSaveData( gamesave )
     local coins = gamesave:get( 'coins' )
     if coins ~= nil then
         self.money = coins
-    end
-    local currently_held = gamesave:get( 'currently_held' )
-    if currently_held then
-        local held = json.decode( currently_held )
-        local ItemClass = require( 'items/item' )
-        local itemNode = {type = held.type, name = held.name, subtype = held.subtype, quantity = held.quantity, MAX_ITEMS = held.MaxItems}
-        local item = ItemClass.new( itemNode )
-        -- HACK: This is a way to equip the weapon before the level is completely loaded
-        -- If we don't defer equipping the item, then an exception will occur regarding the level.collider
-        -- Another option is just to add the item to our inventory
-        Timer.add(1, function()
-            -- After a short delay, equip the weapon
-            item:select( self )
-        end)
     end
 end
 
