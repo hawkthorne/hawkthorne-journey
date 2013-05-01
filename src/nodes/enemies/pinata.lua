0222df912bb528b8cfae9da74c664e71ffc6ec0f
DaNiwa <pham.dany@gmail.com>
2013-05-01 13:00:07 -0400

    Initial Commit

---
+++ src/nodes/enemies/pinata.lua
@@ -0,0 +1,27 @@
+local sound = require 'vendor/TEsound'
+
+return { 
+    name = 'pinata',
+    die_sound = 'acorn_crush',
+    height = 39,
+    width = 19,
+    antigravity = true,
+    hp = 1,
+    damage = 0,
+    peaceful = true,
+    tokens = 5,
+    tokenTypes = { -- p is probability ceiling and this list should be sorted by it, with the last being 1
+        { item = 'coin', v = 1, p = 0.9 },
+        { item = 'health', v = 1, p = 1 }
+    },
+    animations = {
+        default = {
+            left = {'loop', {'1,1'}, 1},
+            right ={'loop', {'1,1'}, 1}
+        },
+        dying = {
+            left = {'loop', {'1,1'}, 1},
+            right ={'loop', {'1,1'}, 1}
+        },
+    }
+}
