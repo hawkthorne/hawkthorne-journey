448cef86a4ad3c172d47b84936978f04706f4487
Caleb John <calebjohn4@gmail.com>
2013-05-16 16:19:10 -0600

    max damage

--- src/nodes/projectiles/lightning.lua
+++ src/nodes/projectiles/lightning.lua
@@ -19,6 +19,7 @@ return{
     stayOnScreen = false,
     thrown = false,
     damage = 4,
+    max_damage = 20,
     horizontalLimit = 600,
     animations = {
         default = {'loop', {'1-3,1'}, 0.1},
@@ -28,7 +29,12 @@ return{
     collide = function(node, dt, mtv_x, mtv_y,projectile)
         if node.isPlayer then return end
         if node.hurt then
-            node:hurt(projectile.damage)
+            if projectile.props.max_damage > 0 then
+                node:hurt(projectile.damage)
+                projectile.props.max_damage = projectile.props.max_damage - projectile.damage
+            else
+                projectile:die()
+            end
         end
     end,
 }
