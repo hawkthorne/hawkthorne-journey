a519bedffd65a4cfabd63a009f963608f2120312
edisonout <edisonouthawkthorne@gmail.com>
2013-05-19 20:22:11 +0100

    adds sparkles

--- src/splash.lua
+++ src/splash.lua
@@ -1,5 +1,6 @@
 local app = require 'app'
 
+local anim8 = require 'vendor/anim8'
 local Gamestate = require 'vendor/gamestate'
 local window = require 'window'
 local fonts = require 'fonts'
@@ -11,6 +12,7 @@ local controls = require 'controls'
 local timer = require 'vendor/timer'
 local menu = require 'menu'
 
+
 function splash:init()
     self.cityscape = love.graphics.newImage("images/menu/cityscape.png")
     self.logo = love.graphics.newImage("images/menu/logo.png")
@@ -20,6 +22,16 @@ function splash:init()
     self.logo_position_final = self.logo:getHeight() / 2 + 40
     self.text = ""
     tween(4, self.logo_position, { y=self.logo_position_final})
+
+    -- sparkles
+    self.sparklesprite = love.graphics.newImage('images/cornelius_sparkles.png')
+    self.bling = anim8.newGrid(24, 24, self.sparklesprite:getWidth(), self.sparklesprite:getHeight())
+    self.sparkles = {{55,34},{42,112},{132,139},{271,115},{274,50}}
+    for _,_sp in pairs(self.sparkles) do
+        _sp[3] = anim8.newAnimation('loop', self.bling('1-4,1'), 0.22 + math.random() / 10) 
+        _sp[3]:gotoFrame( math.random( 4 ) ) 
+    end
+
 
     self.menu = menu.new({ 'start', 'controls', 'options', 'credits', 'exit' })
     self.menu:onSelect(function(option)
@@ -48,9 +60,15 @@ function splash:enter(a)
 end
 
 function splash:update(dt)
+
     if self.double_speed then
         tween.update(dt * 20)
     end
+
+    for _,_sp in pairs(self.sparkles) do
+        _sp[3]:update(dt)
+    end
+
 end
 
 function splash:leave()
@@ -70,10 +88,16 @@ function splash:keypressed( button )
 end
 
 function splash:draw()
+
+    local xlogo = window.width / 2 - self.logo:getWidth()/2
+    local ylogo = window.height / 2 - self.logo_position.y
    
     love.graphics.draw(self.cityscape)
-    love.graphics.draw(self.logo, window.width / 2 - self.logo:getWidth()/2,
-    window.height / 2 - self.logo_position.y)
+    love.graphics.draw(self.logo, xlogo, ylogo )
+
+    for _,_sp in pairs(self.sparkles) do
+        _sp[3]:draw( self.sparklesprite, _sp[1] - 12 + xlogo, _sp[2] - 12 + ylogo )
+    end
 
     if self.logo_position.y >= self.logo_position_final then
       love.graphics.setColor(0, 0, 0)
@@ -90,6 +114,7 @@ function splash:draw()
     end
 
     love.graphics.draw(self.arrow, x + 12, y + 23 + 12 * (self.menu:selected() - 1))
+
 end
 
 return splash
