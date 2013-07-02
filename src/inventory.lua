da35d4a195bd635cbf341c9311eb27e5cee6ebdf
Protuhj <ihatespam234@gmail.com>
2013-07-01 20:21:37 -0400

    Deleted code vs. commenting it out.

--- src/inventory.lua
+++ src/inventory.lua
@@ -433,8 +433,6 @@ function Inventory:drop()
             sound.playSfx('pot_break')
             return
         end
-        -- Uncomment to debug
-        -- inspect(self.pages[self.currentPageName][slotIndex])
         local NodeClass = require('/nodes/' .. itemProps.type)
         itemProps.width = item.image:getWidth()
         itemProps.height = item.image:getHeight() - 15
