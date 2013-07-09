759abf2db7b483452d9bae1b51b15524a99ccfd3
DaNiwa <pham.dany@gmail.com>
2013-07-09 19:10:18 -0400

    Prevents player from picking up enemy objects

--- src/nodes/projectiles/basketball.lua
+++ src/nodes/projectiles/basketball.lua
@@ -13,6 +13,8 @@ return{
     throwVelocityX = 300, 
     throwVelocityY = -200,
     damage = 2,
+    playerCanPickUp = false,
+    enemyCanPickUp = true,
     collide = function(node, dt, mtv_x, mtv_y,projectile)
         if not node.isPlayer then return end
         if projectile.thrown then
