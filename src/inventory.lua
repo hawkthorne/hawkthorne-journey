b6626205aadc04729e422bc17660642ca2b27462
Kyle Conroy <kyle.j.conroy@gmail.com>
2013-08-03 12:58:10 -0700

    Check if consumables exist before adding
    Fix #1685

--- src/inventory.lua
+++ src/inventory.lua
@@ -455,17 +455,21 @@ function Inventory:drop()
         itemProps.x = self.player.position.x + 10
         itemProps.y = self.player.position.y + 24 + (24 - itemProps.height)
         itemProps.properties = {foreground = false}
+
         local myNewNode = NodeClass.new(itemProps, level.collider)
+
+        if myNewNode then
         -- Must set the quantity after creating the Node.
-        myNewNode.quantity = item.quantity or 1
-        assert(myNewNode.draw, 'ERROR: ' .. myNewNode.name ..  ' does not have a draw function!')
-        level:addNode(myNewNode)
-        assert(level:hasNode(myNewNode), 'ERROR: Drop function did not properly add ' .. myNewNode.name .. ' to the level!')--]]
-        self:removeItem(slotIndex, self.currentPageName)
-        if myNewNode.drop then
-            myNewNode:drop(self.player)
+            myNewNode.quantity = item.quantity or 1
+            assert(myNewNode.draw, 'ERROR: ' .. myNewNode.name ..  ' does not have a draw function!')
+            level:addNode(myNewNode)
+            assert(level:hasNode(myNewNode), 'ERROR: Drop function did not properly add ' .. myNewNode.name .. ' to the level!')--]]
+            self:removeItem(slotIndex, self.currentPageName)
+            if myNewNode.drop then
+                myNewNode:drop(self.player)
+            end
+            sound.playSfx('click')
         end
-        sound.playSfx('click')
     end
 end
 
