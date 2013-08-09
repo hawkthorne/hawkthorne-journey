742052bf93a1678180e8323c53ecb7fdd0955636
edisonout <edisonouthawkthorne@gmail.com>
2013-08-09 13:49:54 +0100

    adds throwable node

---
+++ src/nodes/throwables/bottle.lua
@@ -0,0 +1,13 @@
+return{
+    name= 'bottle',
+    type= 'throwable',
+    explode= {
+        frameWidth = 7,
+        frameHeight = 15,
+        animation = {'once', {'1-5,1'}, .10},
+        },
+    height = 15,
+    width = 7,
+    holdXOffset= 7,
+    holdYOffset= 0,
+}   
