60b8c480f70cdde5cc2efb70e79126e69aecb7fb
Kyle Conroy <kyle.j.conroy@gmail.com>
2013-09-04 17:45:48 -0700

    Port the last of the tests over to lunatest

--- src/test/runner.lua
+++ src/test/runner.lua
@@ -5,5 +5,6 @@ lunatest.suite("test/test_tween")
 lunatest.suite("test/test_queue")
 lunatest.suite("test/test_dialog")
 lunatest.suite("test/test_transition")
+lunatest.suite("test/test_fsm")
 
 lunatest.run()
