3832aed0a10651a6649137dda9eb5c2393ee848e
Reddfawks <redpaws@rocketmail.com>
2013-09-07 15:08:23 -0400

    Changed some stock and upped prices

--- src/suppliers/Leslie's Box.lua
+++ src/suppliers/Leslie's Box.lua
@@ -5,32 +5,32 @@ local healthImage = love.graphics.newImage( "images/tokens/health.png" )
 
 return {
     weapons = {
-        {"throwingknife",15,6},
-        {"throwingaxe",12,8},
-        {"mallet",2,350},
-        {"torch",3,120},
-        {"sword",3,100},
-        {"longsword",1,300},
-        {"mace",1,500},
+        {"throwingknife",25,10},
+        {"throwingaxe",30,15},
+        {"mallet",2,400},
+        {"torch",3,200},
+        {"sword",3,175},
+        {"longsword",1,350},
+        {"mace",1,600},
         {"battleaxe",2,500},
-        {"club",6,50},
-        {"boneclub",6,55},
-        {"bow",3,150},
-        {"arrow",30,2},
+        {"boneclub",6,75},
+        {"crimson sword",2,2500},
+        {"bow",3,200},
+        {"arrow",50,5},
 
     },
     materials = {
-        {"leaf",30,30},
-        {"rock",30,20},
-        {"stone",30,35},
-        {"stick",30,15},
-        {"bone",30,50},
-        {"ember",30,70},
+        {"duck",30,70},
+        {"banana",30,100},
+        {"peanut",30,75},
+        {"star",30,80},
+        {"arm",30,90},
+        {"frog",30,70},
     },
     consumables = {
-        {"red_potion",5,80},
+        {"red_potion",5,100},
     },
     misc = {
-        {"lightning",3,300}
+        {"lightning",3,350}
     }
 }
