bbb8c277591620acf6bd7ddb9239fe71e133d810
Philip Horger <campadrenalin@gmail.com>
2013-09-13 23:50:31 -0700

    #1718 Renamed buttons to actions for clarity

--- src/controls.lua
+++ src/controls.lua
@@ -5,19 +5,19 @@ local db = store('controls-1')
 local InputController = {}
 InputController.__index = InputController
 
-function InputController.new(buttonmap)
+function InputController.new(actionmap)
     local controller = {}
     setmetatable(controller, InputController)
 
-    controller.buttonmap = buttonmap or InputController.getButtonMap()
+    controller.actionmap = actionmap or InputController.getActionMap()
     controller:refreshKeymap()
 
     return controller
 end
 
-function InputController.getButtonMap(name)
+function InputController.getActionMap(name)
     -- Classmethod to return a preset table from db
-    local mapname = name or 'buttonmap'
+    local mapname = name or 'actionmap'
     return db:get(mapname, {
         UP = 'up',
         DOWN = 'down',
@@ -32,33 +32,33 @@ function InputController.getButtonMap(name)
 end
 
 function InputController:refreshKeymap()
-    -- Create inverted version of self.buttonmap.
-    -- buttonmap is map[action] == physical_key
+    -- Create inverted version of self.actionmap.
+    -- actionmap is map[action] == physical_key
     -- keymap is map[physical_key] == action
     self.keymap = {}
-    for button, key in pairs(self.buttonmap) do
-        self.keymap[key] = button
+    for action, key in pairs(self.actionmap) do
+        self.keymap[key] = action
     end
 end
 
-function InputController:getButtonmap()
-    -- Display-sanitized copy of self.buttonmap
+function InputController:getActionmap()
+    -- Display-sanitized copy of self.actionmap
     local t = {}
-    for button, _ in pairs(self.buttonmap) do
-        t[button] = self:getKey(button)
+    for action, _ in pairs(self.actionmap) do
+        t[action] = self:getKey(action)
     end
     return t
 end
 
-function InputController:getButton( key )
+function InputController:getAction( key )
     -- Get action for a given physical key
     return self.keymap[key]
 end
 
 -- Only use this function for display, it returns 
 -- key values that love doesn't use
-function InputController:getKey( button )
-    local key = self.buttonmap[button]
+function InputController:getKey( action )
+    local key = self.actionmap[action]
 
     if key == " " then
         return "space"
@@ -67,8 +67,8 @@ function InputController:getKey( button )
     return key
 end
 
-function InputController:isDown( button )
-    local key = self.buttonmap[button]
+function InputController:isDown( action )
+    local key = self.actionmap[action]
 
     if key == nil then
         return false
@@ -77,8 +77,8 @@ function InputController:isDown( button )
     return love.keyboard.isDown(key)
 end
 
--- Returns true if key is available to be assigned to a button.
--- Returns false if key is 'f5' or already assigned to a button.
+-- Returns true if key is available to be assigned to a action.
+-- Returns false if key is 'f5' or already assigned to a action.
 function InputController:keyIsNotInUse(key)
     if key == 'f5' then return false end
     for usedKey, _ in pairs(keymap) do
@@ -87,16 +87,16 @@ function InputController:keyIsNotInUse(key)
     return true
 end
 
--- Reassigns key to button and returns true, or returns false if the key is unavailable.
-function InputController:newButton(key, button)
-    if self:getButton(key) == button then
+-- Reassigns key to action and returns true, or returns false if the key is unavailable.
+function InputController:newAction(key, action)
+    if self:getAction(key) == action then
         return true
     end
 
     if self:keyIsNotInUse(key) then
-        buttonmap[button] = key
+        actionmap[action] = key
         keymap = self:getKeymap()
-        db:set('buttonmap', buttonmap)
+        db:set('actionmap', actionmap)
         db:flush()
         return true
     else
