5011289e05031a3b0ebd19951cd73956adf8ae29
edisonout <edisonouthawkthorne@gmail.com>
2013-09-16 13:56:26 +0100

    adds professor kane character

--- src/select.lua
+++ src/select.lua
@@ -39,7 +39,7 @@ character_selections[3][1][0] = characters['ian']
 character_selections[3][1][1] = characters['rich']
 character_selections[3][1][2] = characters['vicki']
 character_selections[3][0][0] = characters['vaughn']
-character_selections[3][0][1] = characters['kevin']
+character_selections[3][0][1] = characters['kane']
 character_selections[3][0][2] = characters['garrett']
 
 
