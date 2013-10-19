518cb9321238c00bcedf7cbf8e3bc6a6739b34b8
didory123 <didory1356@gmail.com>
2013-10-18 22:19:38 -0700

    new recipes, new prices. Add materials and recipes scattered through out the game

--- src/items/consumables/yellow_potion.lua
+++ src/items/consumables/yellow_potion.lua
@@ -5,6 +5,19 @@ return{
     type = "consumable",
     MAX_ITEMS = 2,
     use = function( consumable, player )
-        player.money = player.money + 25
-    end
+   		local rand = math.random(10)
+        if rand == 1 then
+       		player.money = player.money + 700
+        elseif rand == 2 then
+            player.money = player.money + 200
+        elseif rand == 3 then
+            player.money = player.money + 100
+        elseif rand == 4 or rand == 5 then
+            player.money = player.money + 50
+        elseif rand == 6 or rand == 7 then
+            player.money = player.money + 25
+        else
+            player.money = player.money + 5
+        end
+	end
 }
--- src/items/potion_recipes.lua
+++ src/items/potion_recipes.lua
@@ -6,31 +6,45 @@
 
 return {
     {
-        recipe={stick = 1, rock = 1, arm = 1},
+    --jump boost potion
+        recipe={stone = 3, peanut = 2, frog = 2, eye = 4},
         name='blue_potion',
     },
     {
-        recipe={stick = 1, rock = 1, banana = 1},
+    --invulnerability potion
+        recipe={boulder =2, star = 1, ember = 3, fire = 3},
         name='green_potion',
     },
     {
-        recipe={stick = 1, rock = 1, duck = 1},
+    --speed boost potion
+        recipe={bone = 2, mushroom = 1, duck = 2, rock = 4},
         name='orange_potion',
     },
     {
-        recipe={stick = 1, rock = 1, eye = 1},
+    --max health potion
+        recipe={peanut = 2, frog = 1, arm = 2, ember = 1},
+        name='pink_potion',
+    },
+    {
+    --punch damage potion
+        recipe={stick = 1, rock = 2, leaf = 2},
         name='purple_potion',
     },
     {
-        recipe={eye = 2, arm = 1, frog = 2, star = 4},
+    --health potion
+        recipe={leaf = 2, mushroom = 1, stick = 1, bone = 1},
         name='red_potion',
     },
     {
-        recipe={stick = 1, rock = 1, peanut = 1},
+    --greater health potion
+        recipe={leaf = 3, mushroom = 1, stick = 2, duck = 1},
         name='white_potion',
     },
     {
-        recipe={stick = 1, rock = 1, star = 1},
+    --money potion
+        recipe={boulder = 3, frog = 6, star = 2, fire = 1},
         name='yellow_potion',
     }
 }
+
+
