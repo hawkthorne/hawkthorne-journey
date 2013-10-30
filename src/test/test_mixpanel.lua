ec7ef3c4d88160769ec8693f1d32a5e562a50d2c
Kyle Conroy <kyle@kyleconroy.com>
2013-10-29 18:21:43 -0700

    Add tracking for all scenes. Add a distinct_id for all players

---
+++ src/test/test_mixpanel.lua
@@ -0,0 +1,26 @@
+local mixpanel = require "src/vendor/mixpanel"
+
+--should be -1 for a negative number
+function test_random_id() 
+  assert_equal(#mixpanel.randomId(), 10)
+end
+
+function test_distinct_id() 
+  love.filesystem.remove('mixpanel.txt')
+  assert_equal(mixpanel.distinctId(), mixpanel.distinctId())
+end
+
+function test_distinct_id_len() 
+  love.filesystem.remove('mixpanel.txt')
+  assert_equal(#mixpanel.distinctId(), 10)
+end
+
+function test_distinct_id_source() 
+  love.filesystem.write('mixpanel.txt', 'foo')
+  assert_equal(mixpanel.distinctId(), 'foo')
+end
+
+
+function test_randomness()
+  assert_not_equal(mixpanel.randomId(), mixpanel.randomId())
+end
