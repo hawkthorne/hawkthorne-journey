cc6bf35dcba9f7d2cf1c79b35004a3dd573bc278
DaNiwa <pham.dany@gmail.com>
2013-12-16 20:27:24 -0500

    Fix indentation projectile.lua et al.

--- src/nodes/projectiles/computer.lua
+++ src/nodes/projectiles/computer.lua
@@ -2,31 +2,31 @@ local game = require 'game'
 local Timer = require 'vendor/timer'
 
 return{
-    name = 'computer',
-    type = 'projectile',
-    bounce_factor = 0.1,
-    friction = 0,
-    width = 76,
-    height = 38,
-    frameWidth = 38,
-    frameHeight = 38,
-    velocity = { x=0, y=100 },
-    damage = 0,
-    thrown = true,
-    playerCanPickUp = false,
-    enemyCanPickUp = false,
-    canPlayerStore = false,
-    collide = function(node, dt, mtv_x, mtv_y, projectile)
-    end,
-    collide_end = function(node, dt,projectile)
-    end,
-    floor_collide = function(node,new_y, projectile)
-        projectile.animation = projectile.finishAnimation
-        projectile.collider:setGhost(projectile.bb)
-    end,
-    animations = {
-        default = {'once', {'1,1'}, 1},
-        thrown = {'once', {'1,1'}, 1},
-        finish = {'once', {'2,1'}, 1},
-    }
+  name = 'computer',
+  type = 'projectile',
+  bounce_factor = 0.1,
+  friction = 0,
+  width = 76,
+  height = 38,
+  frameWidth = 38,
+  frameHeight = 38,
+  velocity = { x=0, y=100 },
+  damage = 0,
+  thrown = true,
+  playerCanPickUp = false,
+  enemyCanPickUp = false,
+  canPlayerStore = false,
+  collide = function(node, dt, mtv_x, mtv_y, projectile)
+  end,
+  collide_end = function(node, dt,projectile)
+  end,
+  floor_collide = function(node,new_y, projectile)
+    projectile.animation = projectile.finishAnimation
+    projectile.collider:setGhost(projectile.bb)
+  end,
+  animations = {
+    default = {'once', {'1,1'}, 1},
+    thrown = {'once', {'1,1'}, 1},
+    finish = {'once', {'2,1'}, 1},
+  }
 }
