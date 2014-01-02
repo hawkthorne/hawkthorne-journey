f2fd1302fb8f8146752394ea0f119723f4b93224
Caleb John <calebjohn4@gmail.com>
2014-01-02 00:12:12 -0700

    Adds Stolen torch weapon

---
+++ src/items/weapons/stolen_torch.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------
+-- stolen_torch.lua
+-- The code for the torch, when it is in the player's inventory.
+-----------------------------------------------
+
+return{
+    name = 'stolen_torch',
+    description = 'Torch',
+    type = 'weapon',
+    subtype = 'melee',
+}
