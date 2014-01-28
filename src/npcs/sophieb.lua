e13d33604437553c056932993c0ded0c2de0e51b
Daniel Pham <pham.dany@gmail.com>
2014-01-28 13:42:03 -0500

    Remove reference to deleted music

--- src/npcs/sophieb.lua
+++ src/npcs/sophieb.lua
@@ -43,59 +43,28 @@ return {
             { ['text']='Viva Tacotown!' },
             { ['text']='Village Forest' },
             { ['text']='Valley of Laziness' },
-            { ['text']='To Understand Christmas' },
-            { ['text']='The Way It Goes' },
-            { ['text']='Take The Throne' },
-            { ['text']='Study Room F' },
             { ['text']='Starting The Game' },
             { ['text']='Somewhere Out There' },
             { ['text']='Seabluff' },
-            { ['text']='Running Through Rain' },
-            { ['text']='Roxanne' },
-            { ['text']='Pocketful of Hawthornes' },
             { ['text']='Overworld' },
-            { ['text']='Ol Fashion Nightmare' },
             { ['text']='New Abedtown' },
-            { ['text']='Modern Warfare' },
-            { ['text']='Merry Happy!' },
-            { ['text']='M.A.S.H. Theme' },
-            { ['text']='Mamma Mia' },
-            { ['text']='Love So Alike' },
-            { ['text']='Lindbergh Lean' },
             { ['text']='Lets Play Poker' },
             { ['text']='Lazy Sandpits' },
-            { ['text']='Kiss From A Remix' },
             { ['text']='Kiss From A Rose' },
-            { ['text']='Is He Being Ominous?' },
             { ['text']='Hall O Hippies' },
             { ['text']='Greendale Rave' },
             { ['text']='Greendale Hallways' },
-            { ['text']='Greendale Exterior' },
-            { ['text']='Gravity' },
-            { ['text']='Gilbert Strikes Back' },
-            { ['text']='Getting Rid of Bowser' },
-            { ['text']='Getting Rid of Britta' },
-            { ['text']='Forfeiting' },
+            { ['text']='Greendale Exterior' },            
             { ['text']='Forest' },
-            { ['text']='Finally Be Fine' },
             { ['text']='Farewell Abed' },
             { ['text']='Enter Cornelius' },
-            { ['text']='Die Racism!' },
-            { ['text']='Daylight' },
             { ['text']='Daybreak' },
-            { ['text']='Dancing Queen' },
-            { ['text']='Community Medley' },
             { ['text']='Comfy At A Cauldron' },
             { ['text']='Christmas Infiltration' },
             { ['text']='Castle Interior' },
             { ['text']='Castle Exterior' },
             { ['text']='Blacksmiths House' },
             { ['text']='Black Caverns' },
-            { ['text']='A Winger Speech' },
-            { ['text']='At Least Its Underground' },
-            { ['text']='At Least Its Finally Boss' },
-            { ['text']='At Least It Was Here' },
-            { ['text']='A Simple Question' },
             { ['text']='Alas Poor Britta-Bot' },
             { ['text']='A Girl Milking A Cow' },
             { ['text']='Abeds Christmas Medley' },
@@ -118,9 +87,6 @@ return {
     ["Daybreak"]=function(npc, player)
         playSong(npc, player.currentLevel, 38, "daybreak")
     end,
-    ["A Simple Question"]=function(npc, player)
-        playSong(npc, player.currentLevel, 22.3, "contract")
-    end,
     ["Abeds Christmas Medley"]=function(npc, player)
         playSong(npc, player.currentLevel, 322, "winter")
     end,
@@ -142,14 +108,8 @@ return {
     ["Castle Interior"]=function(npc, player)
         playSong(npc, player.currentLevel, 50.3, "castle")
     end,
-    ["Die Racism!"]=function(npc, player)
-        playSong(npc, player.currentLevel, 89.8, "corneliusbattle")
-    end,
     ["Enter Cornelius"]=function(npc, player)
         playSong(npc, player.currentLevel, 47.5, "cornelius-appears")
-    end,
-    ["Forfeiting"]=function(npc, player)
-        playSong(npc, player.currentLevel, 31, "forfeiting")
     end,
     ["Forest"]=function(npc, player)
         playSong(npc, player.currentLevel, 22.2, "forest")
@@ -160,80 +120,17 @@ return {
     ["Village Forest"]=function(npc, player)
         playSong(npc, player.currentLevel, 33.2, "forest-2")
     end,
-    ["Modern Warfare"]=function(npc, player)
-        playSong(npc, player.currentLevel, 240.5, "modernwarfare")
-    end,
-    ["A Winger Speech"]=function(npc, player)
-        playSong(npc, player.currentLevel, 73.2, "wingerspeech")
-    end,
     ["What Christmas Is For"]=function(npc, player)
         playSong(npc, player.currentLevel, 58.7, "winter2")
-    end,
-    ["The Way It Goes"]=function(npc, player)
-        playSong(npc, player.currentLevel, 228, "wayitgoes")
     end,
     ["New Abedtown"]=function(npc, player)
         playSong(npc, player.currentLevel, 59.1, "abeds-town")
     end,
-    ["At Least It Was Here"]=function(npc, player)
-        playSong(npc, player.currentLevel, 151.3, "leastitwashere")
-    end,
-    ["At Least Its Finally Boss"]=function(npc, player)
-        playSong(npc, player.currentLevel, 274.5, "finallyboss")
-    end,
-    ["At Least Its Underground"]=function(npc, player)
-        playSong(npc, player.currentLevel, 23.4, "atleastunderground")
-    end,
-    ["Merry Happy!"]=function(npc, player)
-        playSong(npc, player.currentLevel, 109.7, "merryhappy")
-    end,
-    ["To Understand Christmas"]=function(npc, player)
-        playSong(npc, player.currentLevel, 74.4, "understandxmas")
-    end,
-    ["Take The Throne"]=function(npc, player)
-        playSong(npc, player.currentLevel, 8.35, "takethethrone")
-    end,
-    ["Running Through Rain"]=function(npc, player)
-        playSong(npc, player.currentLevel, 96.1, "runningthroughrain")
-    end,
-    ["Roxanne"]=function(npc, player)
-        playSong(npc, player.currentLevel, 197, "roxanne")
-    end,
-    ["Lindbergh Lean"]=function(npc, player)
-        playSong(npc, player.currentLevel, 147.1, "lindberghlean")
-    end,
     ["Lets Play Poker"]=function(npc, player)
         playSong(npc, player.currentLevel, 36.5, "tavern")
     end,
-    ["Daylight"]=function(npc, player)
-        playSong(npc, player.currentLevel, 74, "daylight")
-    end,
-    ["Ol Fashion Nightmare"]=function(npc, player)
-        playSong(npc, player.currentLevel, 199, "nightmare")
-    end,
     ["Overworld"]=function(npc, player)
         playSong(npc, player.currentLevel, 20, "overworld")
-    end,
-    ["Christmas Infiltration"]=function(npc, player)
-        playSong(npc, player.currentLevel, 56, "xmasrap")
-    end,
-    ["Love So Alike"]=function(npc, player)
-        playSong(npc, player.currentLevel, 90, "lovesoalike")
-    end,
-    ["Finally Be Fine"]=function(npc, player)
-        playSong(npc, player.currentLevel, 53, "finallybefine")
-    end,
-    ["Getting Rid of Britta"]=function(npc, player)
-        playSong(npc, player.currentLevel, 131.5, "ridbritta")
-    end,
-    ["Getting Rid of Bowser"]=function(npc, player)
-        playSong(npc, player.currentLevel, 76, "bowser")
-    end,
-    ["Gravity"]=function(npc, player)
-        playSong(npc, player.currentLevel, 117.3, "gravity")
-    end,
-    ["M.A.S.H. Theme"]=function(npc, player)
-        playSong(npc, player.currentLevel, 88, "mash-theme")
     end,
     ["Greendale Exterior"]=function(npc, player)
         playSong(npc, player.currentLevel, 40.7, "greendale")
@@ -244,35 +141,14 @@ return {
     ["Greendale Rave"]=function(npc, player)
         playSong(npc, player.currentLevel, 114, "rave")
     end,
-    ["Gilbert Strikes Back"]=function(npc, player)
-        playSong(npc, player.currentLevel, 18.7, "gilbertstrikesback")
-    end,
     ["Lazy Sandpits"]=function(npc, player)
         playSong(npc, player.currentLevel, 136, "sandpits")
-    end,
-    ["Mamma Mia"]=function(npc, player)
-        playSong(npc, player.currentLevel, 204, "mamma-mia")
     end,
     ["Kiss From A Rose"]=function(npc, player)
         playSong(npc, player.currentLevel, 86, "credits")
     end,
-    ["Kiss From A Remix"]=function(npc, player)
-        playSong(npc, player.currentLevel, 101, "kissfromjesus")
-    end,
-    ["Is He Being Ominous?"]=function(npc, player)
-        playSong(npc, player.currentLevel, 60.7, "ominous")
-    end,
-    ["Pocketful of Hawthornes"]=function(npc, player)
-        playSong(npc, player.currentLevel, 70, "pocketful")
-    end,
-    ["Dancing Queen"]=function(npc, player)
-        playSong(npc, player.currentLevel, 237, "dancingqueenfull")
-    end,
     ["Somewhere Out There"]=function(npc, player)
         playSong(npc, player.currentLevel, 132, "somewhereoutthere")
-    end,
-    ["Study Room F"]=function(npc, player)
-        playSong(npc, player.currentLevel, 53.5, "studyroom")
     end,
     ["Seabluff"]=function(npc, player)
         playSong(npc, player.currentLevel, 30.65, "seabluff")
@@ -285,9 +161,6 @@ return {
     end,
     ["Alas Poor Britta-Bot"]=function(npc, player)
         playSong(npc, player.currentLevel, 20, "britta-bot")
-    end,
-    ["Community Medley"]=function(npc, player)
-        playSong(npc, player.currentLevel, 246.1, "medley")
     end,
     ["You Just Lost A Game"]=function(npc, player)
         playSong(npc, player.currentLevel, 7, "you-just-lost")
