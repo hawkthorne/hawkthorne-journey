6e3c980514ea11731b678967029b94e8b8b082be
edisonout <edisonouthawkthorne@gmail.com>
2014-02-08 19:32:08 +0000

    fix spacing

--- src/start.lua
+++ src/start.lua
@@ -11,38 +11,38 @@ local controls  = require('inputcontroller').get()
 local VerticalParticles = require "verticalparticles"
 
 function state:init()
-    VerticalParticles.init()
+  VerticalParticles.init()
 
-    self.background = love.graphics.newImage("images/menu/pause.png")
-    self.arrow = love.graphics.newImage("images/menu/arrow.png")
-    self.option_map = {}
-    self.options = {
-        --           display name       slot number
-        { name = 'SLOT 1',        slot = 1 },
-        { name = 'SLOT 2',        slot = 2 },
-        { name = 'SLOT 3',        slot = 3 },
-    }
-    for i,o in pairs( self.options ) do
-        if o.name then
-            self.option_map[o.name] = self.options[i]
-        end
+  self.background = love.graphics.newImage("images/menu/pause.png")
+  self.arrow = love.graphics.newImage("images/menu/arrow.png")
+  self.option_map = {}
+  self.options = {
+    --           display name       slot number
+    { name = 'SLOT 1',        slot = 1 },
+    { name = 'SLOT 2',        slot = 2 },
+    { name = 'SLOT 3',        slot = 3 },
+  }
+  for i,o in pairs( self.options ) do
+    if o.name then
+      self.option_map[o.name] = self.options[i]
     end
+  end
 
-    self.selection = 0
+  self.selection = 0
 end
 
 function state:update( dt )
-    VerticalParticles.update( dt )
+  VerticalParticles.update( dt )
 end
 
 function state:enter( previous )
-    fonts.set( 'big' )
-    camera:setPosition( 0, 0 )
-    self.previous = previous
+  fonts.set( 'big' )
+  camera:setPosition( 0, 0 )
+  self.previous = previous
 end
 
 function state:leave()
-    fonts.reset()
+  fonts.reset()
 end
 
 function state:startGame(dt)
@@ -76,107 +76,107 @@ end
 -- Gets the saved slot's level name, or the empty string
 -- @param slotNumber the slot number to get the level name for
 function state.get_slot_level(slotNumber)
-    local gamesave = app.gamesaves:all()[ slotNumber ]
-    if gamesave ~= nil then
-        local savepoint = gamesave:get( 'savepoint' )
-        if savepoint ~= nil and savepoint.level ~= nil then
-            -- If the level name is too long, then produce a shortened name for display purposes
-            if savepoint.level:len() > 16 then
-                local shortName = savepoint.level:sub( 0, 11 )
-                shortName = shortName .. '..' .. savepoint.level:sub( -3 )
-                return shortName
-            else
-                return savepoint.level
-            end
-        end
-    else
-        print( "Warning: no gamesave information for slot: " .. slotNumber )
+  local gamesave = app.gamesaves:all()[ slotNumber ]
+  if gamesave ~= nil then
+    local savepoint = gamesave:get( 'savepoint' )
+    if savepoint ~= nil and savepoint.level ~= nil then
+      -- If the level name is too long, then produce a shortened name for display purposes
+      if savepoint.level:len() > 16 then
+        local shortName = savepoint.level:sub( 0, 11 )
+        shortName = shortName .. '..' .. savepoint.level:sub( -3 )
+        return shortName
+      else
+        return savepoint.level
+      end
     end
-    return "<empty>"
+  else
+    print( "Warning: no gamesave information for slot: " .. slotNumber )
+  end
+  return "<empty>"
 end
 
 function state:keypressed( button )
 
-    local option = self.options[ self.selection + 1 ]
+  local option = self.options[ self.selection + 1 ]
 
-    if button == 'START' then
-        if self.previous.name then
-            Gamestate.switch( self.previous )
-        else
-            Gamestate.switch( 'welcome' )
-        end
-        return
-    elseif  button == 'ATTACK' or button == 'JUMP' then
-        sound.playSfx('click')
-        if option.slot then
-            -- Load the selected slot
-            self:load_slot( option.slot )
-        elseif option.action then
-            self[option.action]()
-        end
-    elseif button == 'INTERACT' then
-      sound.playSfx('beep')
-      if self.selection == 0 then
-        love.filesystem.remove('gamesaves-alpha-1.json')
-      elseif self.selection == 1 then
-         love.filesystem.remove('gamesaves-beta-1.json')
-      elseif self.selection == 2 then
-        love.filesystem.remove('gamesaves-gamma-1.json') 
-      end
-    love.event.push("quit")
-    elseif button == 'UP' then
-        sound.playSfx('click')
-        self.selection = (self.selection - 1) % #self.options
-        while self.options[self.selection + 1].name == nil do
-            self.selection = (self.selection - 1) % #self.options
-        end
-    elseif button == 'DOWN' then
-        sound.playSfx('click')
-        self.selection = (self.selection + 1) % #self.options
-        while self.options[self.selection + 1].name == nil do
-            self.selection = (self.selection + 1) % #self.options
-        end
+  if button == 'START' then
+    if self.previous.name then
+      Gamestate.switch( self.previous )
+    else
+      Gamestate.switch( 'welcome' )
     end
+    return
+  elseif  button == 'ATTACK' or button == 'JUMP' then
+    sound.playSfx('click')
+    if option.slot then
+      -- Load the selected slot
+      self:load_slot( option.slot )
+    elseif option.action then
+      self[option.action]()
+    end
+  elseif button == 'INTERACT' then
+    sound.playSfx('beep')
+    if self.selection == 0 then
+      love.filesystem.remove('gamesaves-alpha-1.json')
+    elseif self.selection == 1 then
+      love.filesystem.remove('gamesaves-beta-1.json')
+    elseif self.selection == 2 then
+      love.filesystem.remove('gamesaves-gamma-1.json') 
+    end
+    love.event.push("quit")
+  elseif button == 'UP' then
+    sound.playSfx('click')
+    self.selection = (self.selection - 1) % #self.options
+    while self.options[self.selection + 1].name == nil do
+      self.selection = (self.selection - 1) % #self.options
+    end
+  elseif button == 'DOWN' then
+    sound.playSfx('click')
+    self.selection = (self.selection + 1) % #self.options
+    while self.options[self.selection + 1].name == nil do
+      self.selection = (self.selection + 1) % #self.options
+    end
+  end
 end
 
 function state:draw()
-    VerticalParticles.draw()
+  VerticalParticles.draw()
 
-    love.graphics.setColor(255, 255, 255)
-    local howto = controls:getKey("ATTACK") .. " OR " .. controls:getKey("JUMP") .. ": SELECT SLOT"
-    local delete = controls:getKey("INTERACT") .. ": DELETE SLOT & EXIT"
-    love.graphics.print(howto, 25, 25)
-    love.graphics.print(delete, 25, 55)
-    local yFactor = 20
+  love.graphics.setColor(255, 255, 255)
+  local howto = controls:getKey("ATTACK") .. " OR " .. controls:getKey("JUMP") .. ": SELECT SLOT"
+  local delete = controls:getKey("INTERACT") .. ": DELETE SLOT & EXIT"
+  love.graphics.print(howto, 25, 25)
+  love.graphics.print(delete, 25, 55)
+  local yFactor = 20
 
-    local y = 90
+  local y = 90
 
-    love.graphics.draw(self.background,
-      camera:getWidth() / 2 - self.background:getWidth() / 2,
-      camera:getHeight() / 2 - self.background:getHeight() / 2)
+  love.graphics.draw(self.background,
+    camera:getWidth() / 2 - self.background:getWidth() / 2,
+    camera:getHeight() / 2 - self.background:getHeight() / 2)
 
-    love.graphics.setColor( 0, 0, 0, 255 )
+  love.graphics.setColor( 0, 0, 0, 255 )
 
-    for n, opt in pairs(self.options) do
-        if tonumber( n ) ~= nil  then
-            if opt.name and opt.slot then
-                love.graphics.print( opt.name , 175, y, 0 )
-                y = y + yFactor
-                love.graphics.print( self.get_slot_level( opt.slot ), 190, y, 0 )
-                y = y + yFactor
-            elseif opt.name then
-                y = y + yFactor
-                love.graphics.print( opt.name, 175, y, 0 )
-            end
-        end
+  for n, opt in pairs(self.options) do
+    if tonumber( n ) ~= nil  then
+      if opt.name and opt.slot then
+        love.graphics.print( opt.name , 175, y, 0 )
+        y = y + yFactor
+        love.graphics.print( self.get_slot_level( opt.slot ), 190, y, 0 )
+        y = y + yFactor
+      elseif opt.name then
+        y = y + yFactor
+        love.graphics.print( opt.name, 175, y, 0 )
+      end
     end
-    love.graphics.setColor( 255, 255, 255, 255 )
-    -- Determine how far the arrow should move for the last menu item
-    local arrowYFactor = 2
-    if self.selection > 2 then
-        arrowYFactor = 2.5
-    end
-    love.graphics.draw( self.arrow, 135, 127 + ( (yFactor * arrowYFactor) * ( self.selection - 1 ) ) )
+  end
+  love.graphics.setColor( 255, 255, 255, 255 )
+  -- Determine how far the arrow should move for the last menu item
+  local arrowYFactor = 2
+  if self.selection > 2 then
+    arrowYFactor = 2.5
+  end
+  love.graphics.draw( self.arrow, 135, 127 + ( (yFactor * arrowYFactor) * ( self.selection - 1 ) ) )
 
 end
 
