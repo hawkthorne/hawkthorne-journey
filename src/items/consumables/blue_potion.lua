8e038d3ad3d691fdf906ce4ed5f5587b6b8b8514
Nick Scripp <scrippman@gmail.com>
2014-02-27 11:38:52 -0700

    Add jump to playerEffects and update blue_potion

--- src/items/consumables/blue_potion.lua
+++ src/items/consumables/blue_potion.lua
@@ -1,16 +1,8 @@
 -- made by Nicko21
-local Timer = require 'vendor/timer'
 return{
   name = "blue_potion",
   description = "Jump Boost Potion",
   type = "consumable",
   MAX_ITEMS = 2,
-  duration = 10,
-  use = function( consumable, player )
-    local orig = player.jumpFactor
-    player.jumpFactor = 1.5
-    Timer.add(consumable.props.duration, function() 
-      player.jumpFactor = orig
-    end)
-  end
+  consumable = {jump = {jumpFactor = 1.5, duration = 10}},
 }
--- src/playerEffects.lua
+++ src/playerEffects.lua
@@ -1,9 +1,12 @@
 -- playerEffects.lua
 
+local Timer = require 'vendor/timer'
+
+
 local PlayerEffects = {}
 
 local VALID_EFFECTS = {
-  ['heal']=1,['hurt']=2,['jump']=3,['run']=4,['attack']=5,
+  ['heal']=1,['jump']=2,['speed']=3,['attack']=4,
 }
 
 function PlayerEffects.heal(player, value)
@@ -11,6 +14,24 @@ function PlayerEffects.heal(player, value)
   local healval = math.min(player.max_health - player.health, value)
   player.health = player.health + healval
   return "healed for " .. healval
+end
+
+function PlayerEffects.jump(player, value)
+  local orig = player.jumpFactor
+  player.jumpFactor = value.jumpFactor
+  Timer.add(value.duration, function() 
+    player.jumpFactor = orig
+    table.insert(player.activeEffects,"jump boost expired")
+  end)
+  return "jump boosted by " .. value.jumpFactor .. "x"
+end
+
+function PlayerEffects.speed(player, value)
+
+
+end
+
+function PlayerEffects.EFFECT(player, value)
 end
 
 function PlayerEffects:doEffect(effects, player)
