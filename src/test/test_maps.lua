823695f88b50763c6d7e342fb1d988215dc63f42
AlfonZ42 <AlfonZ42@gmail.com>
2014-03-17 14:20:11 +0100

    Add new test_maps, which tests all reachable levels for referential integrity.
    It starts in (hardcoded) `studyroom` and goes through all doors.
    Checks performed (explicitly):
    - door points to a valid door in a valid level
      Exception: doors can point to no door in other level, as in that case default position is used (position of door `main`); found in:
      - level _womens-bathroom_, door _secret_
      - level _valley_, unnamed door at [149,0]
      - level _black-caverns-2_, unnamed door at [133,7]
      - level _black-caverns-2_, door _seabluff_
      There are couple of doors pointing to _overworld_ (in _new-abedtown_), so there is some special treatment of it.
    - door's property `button` has to be recognized by _inputcontroller_ (currently one of `UP`, `DOWN`, `LEFT`, `RIGHT`, `SELECT`, `START`, `JUMP`, `ATTACK`, `INTERACT`)
    - door's property `sound` has to be `false`/`true` or name of valid sound
    - door's property `key` has to be loadable key, can't test if the key is also placed somewhere in the world, because some keys are loaded dynamically (e.g. white_crystal)
    - level's property `music` has to be name of valid music
    
    Loading a level checks references to other things implicitly, e.g. tilesets images.
    
    Level's field `map` is currently not needed for testing, so it's being set to `nil`, lowering memory consumption.
    
    All levels are loaded first, then the checks are performed, so checking of doors doesn't cause multiple loads of the same level(s).
    Also, this enables separating different tests (i.e. doors from music and possible future tests).
    
    Another option would be to load a level, perform all possible checks immediately (except doors check), store information needed for doors check and load next level in the queue.
    This should lower memory consumption even more, at the expense of new data structures.
    
    Sounds and musics are being tested in batches - they are collected first and tested afterwards.
    This improves error message in case of an error - all sources referring to failed sound/music are reported.

---
+++ src/test/test_maps.lua
@@ -0,0 +1,260 @@
+local controls = require("inputcontroller").get()
+local Level = require "level"
+local Sound = require 'vendor/TEsound'
+local utils = require "utils"
+
+-- all loaded levels
+local levels = nil
+
+---
+-- Custom protected mode caller
+-- so (regular) asserts are caught and we have the chance to report them in a nicer way
+-- @param f error-protected function
+-- @return same as xpcall
+local function myxpcall(f)
+  local err_handler = function(e)
+    return debug.traceback(tostring(e), 2);
+  end
+
+  return xpcall(f, err_handler)
+end
+
+---
+-- Adds key-value pair into the supplied table
+-- @param t associative array with multiple values
+-- @param key
+-- @param value
+-- @return nil
+local function addItem(t, key, value)
+  assert(type(t) == "table" and key ~= nil and value ~= nil)
+  if t[key] == nil then
+    t[key] = { value }
+  elseif not utils.contains(t[key], value) then
+    table.insert(t[key], value)
+  end
+end
+
+---
+-- Gets user-friendly identifier of all items in the supplied table
+-- @param t table
+-- @return string
+local function getUserIds(t)
+  assert(type(t) == "table")
+  local ids = {}
+  for _,item in pairs(t) do
+    table.insert(ids, item:getUserId())
+  end
+  return table.concat(ids, "; ")
+end
+
+---
+-- Loads all levels reachable from 'studyroom' into {@link levels}
+-- @return nil
+local function loadLevels()
+  if levels then return end
+
+  levels = {}
+
+  ---
+  -- Loads target level, with improved error message in case of an error
+  -- @param todoItem
+  -- @return Level
+  local function getLevel(todoItem)
+    assert(todoItem ~= nil and type(todoItem.toLevel) == "string")
+    local f = function ()
+      return Level.new(todoItem.toLevel)
+    end
+    local ok, msg = myxpcall(f)
+    if not ok then
+      local levelRef = (todoItem.fromDoor ~= nil) and todoItem.fromDoor:getUserId() or ("level " .. todoItem.fromLevel)
+      fail(string.format("Error loading level '%s' (referenced from %s) - %s", todoItem.toLevel, levelRef, msg))
+      return nil
+    else
+      return msg
+    end
+  end
+
+  ---
+  -- Adds item into the table
+  -- @param t table
+  -- @item
+  -- @return nil
+  local function addTodoItem(t, item)
+    assert(type(t) == "table" and item ~= nil and item.toLevel ~= nil and item.fromLevel ~= nil)
+    table.insert(t, item)
+  end
+
+  ---
+  -- Removes first item in the table and returns it
+  -- @param t table
+  -- @return todoItem
+  local function removeTodoItem(t)
+    assert(type(t) == "table" and #t > 0)
+    local item = table.remove(t, 1)
+    return item
+  end
+
+  -- comparator function for determining if supplied todoItems refer to the same level
+  local compTodoLevel = function (item1, item2)
+    assert(item1 ~= nil and item2 ~= nil)
+    return item1.toLevel == item2.toLevel
+  end
+
+  -- queue of levels waiting for processing
+  -- each item is table:
+  --   item.toLevel string - name of the target level [required]
+  --   item.fromLevel string - name of the source level (for reporting purposes in case of errors) [required]
+  --   item.fromDoor Door - door in the source level (for reporting purposes in case of errors) [optional]
+  local levelNamesTodo = {}
+  addTodoItem(levelNamesTodo, {toLevel="studyroom", fromLevel="(default)"})
+
+  while #levelNamesTodo > 0 do
+    local levelTodo = removeTodoItem(levelNamesTodo)
+    local levelName = levelTodo.toLevel
+
+    -- overworld is special, can't be processed like common level
+    if levelName ~= "overworld" then
+      local level = getLevel(levelTodo)
+      -- loading all levels requires quite a lot of memory
+      -- map field is currently not needed for testing, set it to nil to free its memory
+      level.map = nil
+
+      local doors = level:getOutgoingDoors()
+      -- skip levels already reached
+      for _,door in ipairs(doors) do
+        local targetLevelname = door.level
+        if targetLevelname ~= levelName and levels[targetLevelname] == nil and not utils.containsComp(levelNamesTodo, {toLevel=targetLevelname}, compTodoLevel) then
+          addTodoItem(levelNamesTodo, {toLevel=targetLevelname, fromLevel=levelName, fromDoor=door})
+        else
+        end
+      end
+
+      levels[levelName] = level
+    else
+      --local levelRef = (levelTodo.fromDoor ~= nil) and levelTodo.fromDoor:getUserId() or ("level " .. levelTodo.fromLevel)
+      --print(string.format("Warning: Door pointing to overworld - %s", levelRef))
+    end
+  end
+end
+
+---
+-- Checks supplied door
+-- - target level and door has to exist (or be empty)
+-- - button has to be recognized by inputcontroller
+-- - sound has to be false/true or name of valid sound
+-- - key has to be loadable key; can't test if key is also placed somewhere in the world, because some keys are loaded dynamically (e.g. white_crystal)
+-- @param door Door
+-- @param sounds associative array with multiple values - collected sounds from doors for later testing
+-- @return nil
+local function checkDoor(door, sounds)
+  if door.level ~= nil then
+    local targetLevelName = door.level
+    if targetLevelName == "overworld" then
+      -- skip check
+    else
+      local targetLevel = levels[targetLevelName]
+      assert_not_nil(targetLevel, string.format("Level %s not found. Referenced from %s.", targetLevelName, door:getUserId()))
+      local targetDoorName = door.to
+      if targetDoorName == nil then
+        -- allow doors with no target door names into other level and assume 'main'
+        if door.containerLevel ~= nil and door.containerLevel.name ~= targetLevelName then
+          --print(string.format("Warning: Target door name is unspecified - %s.", door:getUserId()))
+          targetDoorName = "main"
+        else
+          fail(string.format("Target door name into the same level is unspecified - %s.", door:getUserId()))
+        end
+      end
+      assert_true(targetDoorName ~= "", string.format("Target door name is empty - %s.", door:getUserId()))
+      local targetDoor = targetLevel.doors[targetDoorName]
+      assert_not_nil(targetDoor, string.format("Door '%s' not found in level %s. Referenced from %s.", targetDoorName, targetLevelName, door:getUserId()))
+    end
+  end
+
+  do
+    local actionmap = controls:getActionmap()
+    local button = door.button
+    assert_not_nil(actionmap[button], string.format("Value '%s' of door's property button not recognized (%s).", tostring(button), door:getUserId()))
+  end
+
+  if type(door.sound) ~= 'boolean' then
+    addItem(sounds, door.sound, door)
+  end
+
+  if door.key ~= nil then
+    local keyName = door.key
+    local f = function ()
+      return utils.require('items/keys/' .. keyName)
+    end
+    local ok, msg = myxpcall(f)
+
+    if not ok then
+      fail(string.format("Error loading key '%s'. Referenced from %s - %s.", keyName, door:getUserId(), msg))
+    end
+    assert_not_nil(msg, string.format("Key '%s' not found. Referenced from %s.", keyName, door:getUserId()))
+  end
+end
+
+---
+-- Tests referential integrity of doors of all levels
+function test_maps_doors()
+  loadLevels()
+  assert_gt(0, utils.propcount(levels), "No levels loaded")
+
+  -- collected doors' sound properties (associative array with multiple values)
+  -- don't test sounds for each door, multiple doors can use the same sound
+  --   in case of error it provides better information
+  local sounds = {}
+
+  for levelname,level in pairs(levels) do
+    for _,door in pairs(level.nodes) do
+      if door.isDoor then
+        checkDoor(door, sounds)
+      end
+    end
+  end
+
+  local oldSoundDisabled = Sound.disabled
+  Sound.disabled = false
+  -- check collected sounds
+  for sound,nodes in pairs(sounds) do
+    local f = function ()
+      Sound.playSfx(sound)
+    end
+    local ok, msg = myxpcall(f)
+    if not ok then
+      local ids = getUserIds(nodes)
+      fail(string.format("Error playing sound '%s' (referenced from doors: %s) - %s", sound, ids, msg))
+    end
+  end
+  Sound.disabled = oldSoundDisabled
+end
+
+---
+-- Tests playability of musics of all levels
+function test_maps_music()
+  loadLevels()
+  assert_gt(0, utils.propcount(levels), "No levels loaded")
+
+  -- collected levels' musics
+  -- don't test music for each level, multiple levels can use the same music
+  local musics = {}
+  for levelname,level in pairs(levels) do
+    addItem(musics, level.music, level)
+  end
+
+  local oldSoundDisabled = Sound.disabled
+  Sound.disabled = false
+  -- check collected musics
+  for music,levels in pairs(musics) do
+    local f = function ()
+      Sound.playMusic(music)
+    end
+    local ok, msg = myxpcall(f)
+    if not ok then
+      local ids = getUserIds(levels)
+      fail(string.format("Error playing music '%s' (referenced from levels: %s) - %s", music, ids, msg))
+    end
+  end
+  Sound.stopMusic()
+  Sound.disabled = oldSoundDisabled
+end
