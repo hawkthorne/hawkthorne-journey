38fe22d6b1badd7098c8e19c503c43b7d11266ca
DaNiwa <pham.dany@gmail.com>
2014-03-28 16:19:15 -0400

    update template conf

--- templates/conf.lua
+++ templates/conf.lua
@@ -4,9 +4,9 @@ function love.conf(t)
     t.author            = "https://github.com/hawkthorne?tab=members"
     t.version           = "0.8.0"
     t.identity          = "hawkthorne_release"
-    t.screen.width      = 1056
-    t.screen.height     = 672
-    t.screen.fullscreen = false
+    t.window.width      = 1056
+    t.window.height     = 672
+    t.window.fullscreen = false
     t.console           = false
     t.modules.physics   = false
     t.modules.joystick  = false
