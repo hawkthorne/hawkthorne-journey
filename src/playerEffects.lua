2a4acc76d01e1b53a739c2aa4e39a646228b7b14
Nick Scripp <scrippman@gmail.com>
2014-04-14 17:54:08 -0600

    Update tacomeat to use playerEffects

--- src/playerEffects.lua
+++ src/playerEffects.lua
@@ -27,22 +27,42 @@ function PlayerEffects.heal(player, value)
   return "healed for " .. healval
 end
 
-function PlayerEffects.jump(player, value)
-  local orig = player.jumpFactor
-  player.jumpFactor = value.jumpFactor
-  Timer.add(value.duration, function() 
-    player.jumpFactor = orig
-    HUDMessage("jump boost expired", player)
+function PlayerEffects.buff(player, buff)
+  local orig = player[buff.attribute]
+  player[buff.attribute] = buff.value
+  Timer.add(buff.duration, function()
+    player[buff.attribute] = orig
+    HUDMessage(buff.endMessage, player)
   end)
-  return "jump boosted by " .. value.jumpFactor .. "x"
+  return buff.startMessage .. (buff.startMessageValue and buff.value or "")
 end
 
-function PlayerEffects.speed(player, value)
-  local orig = player.speedFactor
-  player.speedFactor = value.speedFactor
-  Timer.add(value.duration, function() 
-    player.speedFactor = orig
-    HUDMessage("speed boost expired", player)
+function PlayerEffects.zombie(player)
+  local punchDamage = player.punchDamage
+  local jumpDamage = player.jumpDamage
+  local slideDamage = player.slideDamage
+  local costume = player.character.costume
+  Timer.add(66, function () --Resets damage boost and costume after one minute being active
+    HUDMessage("a chilling gust of AC makes you forget your hunger for brains", player, 10)
+    player.punchDamage = punchDamage
+    player.jumpDamage = jumpDamage
+    player.slideDamage = slideDamage
+    player.character.costume = costume
+  end)
+  HUDMessage("that taco meat tastes weird...", player)
+  for i=1,2 do
+    Timer.add(2*i-1, function () -- Damage over time
+      if player.health > 1 then player:hurt(15) end
+    end)
+  end
+  Timer.add(6, function () -- Set costume to zombie and double unarmed player damage.
+    if love.filesystem.exists("images/characters/" .. player.character.name .. "/zombie.png") then
+      player.character.costume = 'zombie'
+    end
+    HUDMessage("holy crap, you are a zombie!", player, 10)
+    player.jumpDamage = player.jumpDamage * 2
+    player.punchDamage = player.punchDamage * 2
+    player.slideDamage = player.slideDamage * 2
   end)
 end
 
