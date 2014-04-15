886066bbc509c94a6b1ccaf4d1b35d7a1b367344
Nick Scripp <scrippman@gmail.com>
2014-04-14 19:15:40 -0600

    Add background to HUD message

--- src/hud.lua
+++ src/hud.lua
@@ -104,8 +104,11 @@ function HUD:draw( player )
   love.graphics.print(player.money, self.x + 69, self.y + 41,0,0.5,0.5)
   love.graphics.print(player.character.name, self.x + 60, self.y + 15,0,0.5,0.5)
   if player.activeEffects then
+    love.graphics.setColor (120, 120, 120, 255)
+    love.graphics.rectangle("fill", self.x + 15, self.y + 55, 180, 21 * #player.activeEffects)
+    love.graphics.setColor( 0, 0, 0, 255 )
     for i,effect in ipairs(player.activeEffects) do
-      love.graphics.print(effect, self.x + 20, self.y + 50 + (20 * i),0,0.5,0.5)
+      love.graphics.printf(effect, self.x + 20, self.y + 40 + (20 * i), 350, "left",0,0.5,0.5)
     end
   end
   love.graphics.setColor( 255, 255, 255, 255 )
