4f159badd8c9330b67a27dc099f3058e73834b2a
edisonout <edisonouthawkthorne@gmail.com>
2014-05-06 19:46:12 +0100

    zordy stuff

---
+++ src/nodes/zords/dinosaur.lua
@@ -0,0 +1,13 @@
+return{
+    name= 'dinosaur',
+    type= 'zord',
+    explode= {
+        frameWidth = 324,
+        frameHeight = 194,
+        animation = {'loop', {'1,3-4'}, .10},
+        },
+    height = 194,
+    width = 324,
+    holdXOffset= 120,
+    holdYOffset= 30,
+}
