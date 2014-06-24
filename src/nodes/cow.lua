9afb68f5209d3f4eb7568af632fe174c00686006
Brendon Walsh <brendonwalsh@niamu.com>
2014-06-24 16:59:23 -0400

    Cow in town dies after the blacksmith is killed.

--- src/nodes/cow.lua
+++ src/nodes/cow.lua
@@ -1,4 +1,5 @@
 local anim8 = require 'vendor/anim8'
+local app = require 'app'
 
 local Cow = {}
 
@@ -7,7 +8,7 @@ Cow.__index = Cow
 local image = love.graphics.newImage('images/sprites/town/cow.png')
 image:setFilter('nearest', 'nearest')
 
-local g = anim8.newGrid(84,60, image:getWidth(), image:getHeight())
+local g = anim8.newGrid(114,60, image:getWidth(), image:getHeight())
 
 local states = {
     left = anim8.newAnimation('once',g('1,1'), 1),
@@ -17,6 +18,7 @@ local states = {
     up_right = anim8.newAnimation('once',g('2,2'), 1),
     straight = anim8.newAnimation('once',g('1,3'), 1),
     blink = anim8.newAnimation('once',g('2,3'), 1),
+    dead = anim8.newAnimation('once',g('4,3'), 1),
 }
 
 function Cow.new(node, collider)
@@ -29,7 +31,7 @@ function Cow.new(node, collider)
     cow.height = node.height
     
     cow.offset = {
-        x = -50,
+        x = -80,
         y = 0
     }
     
@@ -42,32 +44,37 @@ function Cow.new(node, collider)
 end
 
 function Cow:update(dt, player)
-    self.blinkdelay = self.blinkdelay + dt
-    if self.blinkdelay >= self.nextblink then
-        self.state = 'blink'
-        if self.blinkdelay >= self.nextblink + 0.5 then
-            self.state = self.looking
-            self.nextblink = ( math.random(30) / 10 ) + 2 -- between 2 and 5 seconds
-            self.blinkdelay = 0
-        end
-    elseif player.position.x <= self.x + self.offset.x then
-        self.looking = 'left'
-        if player.position.y <= self.y + self.offset.y then
-            self.looking = 'up_left'
-        end
-        self.state = self.looking
-    elseif player.position.x >= self.x + self.width + self.offset.x then
-        self.looking = 'right'
-        if player.position.y <= self.y + self.offset.y then
-            self.looking = 'up_right'
-        end
-        self.state = self.looking
+    local gamesave = app.gamesaves:active()
+    if gamesave:get('blacksmith-dead', false) then
+        self.state = 'dead'
     else
-        self.looking = 'straight'
-        if player.position.y <= self.y + self.offset.y then
-            self.looking = 'up'
+        self.blinkdelay = self.blinkdelay + dt
+        if self.blinkdelay >= self.nextblink then
+            self.state = 'blink'
+            if self.blinkdelay >= self.nextblink + 0.5 then
+                self.state = self.looking
+                self.nextblink = ( math.random(30) / 10 ) + 2 -- between 2 and 5 seconds
+                self.blinkdelay = 0
+            end
+        elseif player.position.x <= self.x + self.offset.x then
+            self.looking = 'left'
+            if player.position.y <= self.y + self.offset.y then
+                self.looking = 'up_left'
+            end
+            self.state = self.looking
+        elseif player.position.x >= self.x + self.width + self.offset.x then
+            self.looking = 'right'
+            if player.position.y <= self.y + self.offset.y then
+                self.looking = 'up_right'
+            end
+            self.state = self.looking
+        else
+            self.looking = 'straight'
+            if player.position.y <= self.y + self.offset.y then
+                self.looking = 'up'
+            end
+            self.state = self.looking
         end
-        self.state = self.looking
     end
 end
 
@@ -76,5 +83,3 @@ function Cow:draw()
 end
 
 return Cow
-
-
