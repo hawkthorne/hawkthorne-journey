e4060d43614d199aa425e77f7be4e000545480b6
8bitgentleman <mainmoniker@googlemail.com>
2014-07-31 15:35:35 -0400

    first bit of communityfan1107's code
    needs to be fleshed out

--- src/npcs/townsperson.lua
+++ src/npcs/townsperson.lua
@@ -1,4 +1,5 @@
 -- inculdes
+local Timer = require('vendor/timer')
 
 return {
     width = 32,
@@ -9,8 +10,13 @@ return {
         },
         walking = {
             'loop',{'1,1','2,1','3,1'},.2,
- 
                 },
+        rave =  {
+            'loop',{'5,1','6,1','7,1'},.15,
+        },
+        love =  {
+            'loop',{'8,1','9,1'},.10,
+        },
   },
  
     walking = true,
@@ -78,7 +84,47 @@ return {
         "I just do",
         "Dont question",
         },
+    ["marry"] ={
+    	"I dont roll that way.",
+    	"If you wanna find someone for a person like you,",
+    	"Take a trip to gay island.",
+    },
+    ["directions"] ={
+        "up, up, down, down",
+        "oh %&$* i am lost",
+    },
 },
- 
-     
+
+  command_items = { 
+        { ['text']='die' }, 
+        { ['text']='directions'},
+        { ['text']='love'},
+        { ['text']='rave'},   
+     },
+
+	command_commands = {   
+        ['rave']=function(npc, player)
+        	npc.walking = false
+        	npc.stare = false
+        	npc.state = "rave"
+        	npc.busy = true
+        	Timer.add(5, function()
+				npc.state = "walking"
+				npc.busy = false
+				npc.walking = true
+        	end)
+    	end,
+
+		['love']=function(npc, player)
+			npc.walking = false
+			npc.stare = false
+			npc.state = "love"
+			npc.busy = true
+			Timer.add(3, function()
+				npc.state = "walking"
+				npc.busy = false
+				npc.walking = true
+			end)
+		end,
+	} 
 }
