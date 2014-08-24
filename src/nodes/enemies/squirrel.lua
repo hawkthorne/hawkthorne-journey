49dd97a3a902089a54be1df74a8ffcfe942955cf
8bitgentleman <mainmoniker@googlemail.com>
2014-08-24 14:34:12 -0400

    Add squirrel death sound and drippy platforms

--- src/nodes/enemies/squirrel.lua
+++ src/nodes/enemies/squirrel.lua
@@ -6,7 +6,7 @@ local Gamestate = require 'vendor/gamestate'
 return {
   name = 'squirrel',
   --attack_sound = 'manicorn_running',
-  die_sound = 'manicorn_neigh',
+  die_sound = 'squeek',
   position_offset = { x = 0, y = 0 },
   height = 30,
   width = 44,
