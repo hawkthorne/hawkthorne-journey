127ab8e43696313b15dd496fad7aa848fb98ef4e
8bitgentleman <mainmoniker@googlemail.com>
2014-08-25 19:31:07 -0400

    add question mark

--- src/nodes/info.lua
+++ src/nodes/info.lua
@@ -1,4 +1,5 @@
 local Dialog = require 'dialog'
+local anim8 = require 'vendor/anim8'
 local utils = require 'utils'
 
 local Info = {}
@@ -13,16 +14,27 @@ function Info.new(node, collider)
     info.bb = collider:addRectangle(node.x, node.y, node.width, node.height)
     info.bb.node = info
     info.info = utils.split(node.properties.info, '|')
+    info.dt = math.random()
 
     info.x = node.x
     info.y = node.y
     info.height = node.height
     info.width = node.width
     info.position = { x = node.x, y = node.y }
+    info.animation = node.properties.animation or false
 
     if node.properties.sprite ~= nil then
         info.sprite = love.graphics.newImage('images/info/'.. node.properties.sprite ..'.png')
     end
+    if info.animation then
+		info.flip = node.properties.flip == 'true'
+		info.speed = node.properties.speed and tonumber(node.properties.speed) or 0.20
+		
+		local g = anim8.newGrid(tonumber(node.width), tonumber(node.height), 
+		                                info.sprite:getWidth(), info.sprite:getHeight())
+		
+		info.animation = anim8.newAnimation( 'loop', g( unpack( utils.split( node.properties.animation, '|' ) ) ), info.speed )
+	end
 
     collider:setPassive(info.bb)
     
@@ -32,11 +44,18 @@ function Info.new(node, collider)
 end
 
 function Info:update(dt, player)
+	self.dt = self.dt + dt
+		
+	if self.animation then
+		self.animation:update(dt)
+	end
 end
 
 function Info:draw()
-    if self.sprite ~= nil then
-        love.graphics.draw(self.sprite, self.position.x, self.position.y)
+	if self.sprite ~= nil and self.animation then
+    	self.animation:draw(self.sprite, self.x, self.y, 0, self.flip and -1 or 1, 1, self.flip and self.width or 0)
+    elseif self.sprite ~= nil and not self.animation then
+		love.graphics.draw(self.sprite, self.position.x, self.position.y)
     end
 end
 
