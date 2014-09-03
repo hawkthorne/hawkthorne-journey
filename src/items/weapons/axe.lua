6897b769ef73930f1b67b506aacb59d5b3ca3c82
8bitgentleman <mainmoniker@googlemail.com>
2014-09-03 09:20:44 -0400

    nunchucks and axe

---
+++ src/items/weapons/axe.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------
+-- maceItem.lua
+-- The code for the mace, when it is in the player's inventory.
+-- Created by NimbusBP1729
+-----------------------------------------------
+
+return{
+    name = 'axe',
+    description = "Axe",
+    type = 'weapon',
+    subtype = 'melee',
+    damage = 4,
+	special_damage = 'slash= 1, axe= 1',
+	info = 'a standard axe, probally used for chopping wood',
+}
---
+++ src/items/weapons/nunchucks.lua
@@ -0,0 +1,15 @@
+-----------------------------------------------
+-- swordItem.lua
+-- The code for the sword, when it is in the player's inventory.
+-- Created by NimbusBP1729
+-----------------------------------------------
+
+return{
+    name = 'nunchucks',
+    description = 'Nunchucks',
+    type = 'weapon',
+    subtype = 'melee',
+    info = 'a deadly pair of nunchucks',
+    damage = 2,
+    special_damage = 'blunt= 1',
+}
