881d84da5d625e33fe39df05ef5ad87aeeb7c681
8bitgentleman <mainmoniker@googlemail.com>
2014-09-22 22:36:39 -0400

    add code text and likeness page

--- src/likeness_fail.lua
+++ src/likeness_fail.lua
@@ -8,8 +8,7 @@ local VerticalParticles = require "verticalparticles"
 
 local autosave_warning = Gamestate.new()
 
-local savingImage = love.graphics.newImage('images/hud/saving.png')
-savingImage:setFilter('nearest', 'nearest')
+
 
 function autosave_warning:init( )
     VerticalParticles.init()
@@ -17,8 +16,9 @@ end
 
 function autosave_warning:enter( prev )
 
-  local h = anim8.newGrid(36, 36, savingImage:getWidth(), savingImage:getHeight())
-  self.savingAnimation = anim8.newAnimation('loop', h('1-8,1'), .25)
+  
+  self.camera = love.graphics.newImage("images/camera.png")
+  self.splash = love.graphics.newImage("images/cameramenu.png")
   Timer.add(8, function()
     Gamestate.switch('scanning')
   end)
@@ -31,30 +31,30 @@ end
 function autosave_warning:draw()
   VerticalParticles.draw()
   love.graphics.setColor( 255, 255, 255, 255 )
-  local top = (window.height - 36)/2 
-  local side = (window.width - 36)/2
-  self.savingAnimation:draw(savingImage, side, top)
+  local top = (window.height - 300)/2 
+  local side = (window.width - 400)/2
+  love.graphics.draw(self.splash, side, top)
+  love.graphics.draw(self.camera, ((window.width - 150)/2), ((window.height - 120)/2))
   love.graphics.setColor( 255, 255, 255, 255 )
-  local warn1 = 'This game utilizes an autosave feature.'
+  local warn1 = 'A photograph of you will be taken and  \n\n an avatar made of your likeness.'
   local offset1 = (window.width - love.graphics.getFont():getWidth(warn1))/2
   love.graphics.print(warn1, offset1, top + 36)
-  local warn2 = 'It will run each time you reach a new level or checkpoint.'
-  local offset2 = (window.width - love.graphics.getFont():getWidth(warn2))/2
-  love.graphics.print(warn2, offset2, top + 54)
-  local warn3 = 'You can also manually save in the options if ever you\'re in a hurry.'
+  --local warn2 = 'It will run each time you reach a new level or checkpoint.'
+  --local offset2 = (window.width - love.graphics.getFont():getWidth(warn2))/2
+  --love.graphics.print(warn2, offset2, top + 54)
+  local warn3 = 'Don\'t smile.'
   local offset3 = (window.width - love.graphics.getFont():getWidth(warn3))/2
-  love.graphics.print(warn3, offset3, top + 72)
+  love.graphics.print(warn3, offset3, top + 272)
 
 end
 
 function autosave_warning:keypressed(button)
   Timer.clear()
-  Gamestate.switch('likeness_fail')
+  Gamestate.switch('scanning')
 end
 
 function autosave_warning:update(dt)
   VerticalParticles.update(dt)
-  self.savingAnimation:update(dt)
 end
 
 return autosave_warning
--- src/welcome.lua
+++ src/welcome.lua
@@ -6,11 +6,12 @@ local Gamestate = require 'vendor/gamestate'
 local menu      = require 'menu'
 local sound = require 'vendor/TEsound'
 local window    = require 'window'
+local Timer = require 'vendor/timer'
 
 local state = Gamestate.new()
 
 function state:init()
-
+--
   self.menu = menu.new({ 'start', 'controls', 'options', 'credits', 'exit' })
   self.menu:onSelect(function(option)
     if option == 'exit' then
@@ -25,9 +26,8 @@ function state:init()
 end
 
 function state:enter(previous)
-  self.splash = love.graphics.newImage("images/openingmenu.png")
-  self.arrow = love.graphics.newImage("images/menu/small_arrow.png")
-  self.text = string.format(app.i18n('s_or_s_select_item'), controls:getKey('JUMP'), controls:getKey('ATTACK') )
+  
+  self.text = string.format(app.i18n('s_or_s_select_quit'), controls:getKey('JUMP'), controls:getKey('START') )
   self.bg = sound.playMusic("ending")
 
   self.line = " terminal:// \n\n operations://loadprogram:(true) \n\n"..
@@ -35,22 +35,62 @@ function state:enter(previous)
   self.line_short = ""
   self.line_count = 1
   self.line_timer = 0
+  self.code = " 5465415151 \n\n 5465415151 \n\n".. " 5465415151 \n\n 1651094097 \n\n 9046515154 \n\n 5490490456 \n\n 1571754564 \n\n 8940186515 \n\n 6454040940 \n\n 1651321894 \n\n 8979046541 \n\n 1216547789 \n\n 4651561165 \n\n 1121894897 \n\n 2278578589 \n\n 2857868678 \n\n 8678668287 \n\n 2784785786 \n\n 7868767867 \n\n 8678678678 \n\n 6786786786 \n\n 7867867867 \n\n 9447944794 \n\n 4794479447"
+  self.code_short = ""
+  self.code_short2 = ""
+  self.code_short3 = ""
+  self.code_short4 = ""
+  self.code_short5 = ""
+  self.code_count = 1
+  self.code_count2 = 1
+  self.code_count3 = 1
+  self.code_count4 = 1
+  self.code_count5 = 1
+  self.code_timer = 0
 
   self.previous = previous
 end
 
 function state:keypressed( button )
   self.menu:keypressed(button)
+  if button == 'START' then
+        love.event.push("quit")
+  end
 end
 
 function state:update(dt)
   
    self.line_timer = self.line_timer + dt
+   self.code_timer = self.code_timer + dt
    if self.line_timer > 0.05 then
     self.line_timer = 0
     self.line_short = self.line_short..self.line.sub(self.line, self.line_count, self.line_count)
     self.line_count = self.line_count + 1
-  end
+    if self.line_count >135 then
+      if self.code_timer > 0.005 then
+        self.code_timer = 0
+        self.code_short = self.code_short..self.code.sub(self.code, self.code_count, self.code_count)
+        self.code_count = self.code_count + 1
+        if self.code_count >23 then
+          self.code_short2 = self.code_short2..self.code.sub(self.code, self.code_count2, self.code_count2)
+          self.code_count2 = self.code_count2 + 1
+        end
+        if self.code_count >46 then
+          self.code_short3 = self.code_short3..self.code.sub(self.code, self.code_count3, self.code_count3)
+          self.code_count3 = self.code_count3 + 1
+        end
+        if self.code_count >69 then
+          self.code_short4 = self.code_short4..self.code.sub(self.code, self.code_count4, self.code_count4)
+          self.code_count4 = self.code_count4 + 1
+        end
+        if self.code_count >92 then
+          self.code_short5 = self.code_short5..self.code.sub(self.code, self.code_count5, self.code_count5)
+          self.code_count5 = self.code_count5 + 1
+        end
+      end
+    end
+   end
+
 end
 
 function state:draw()
@@ -64,21 +104,22 @@ function state:draw()
   fonts.set('courier')
   love.graphics.setColor( 48, 254, 31, 225 )
   love.graphics.print(self.line_short, 50, 50, 0, 0.5, 0.5 )
+  love.graphics.print(self.code_short, 60, 130, 0, 0.4, 0.4 )
+  love.graphics.print(self.code_short2, 120, 130, 0, 0.4, 0.4 )
+  love.graphics.print(self.code_short3, 180, 130, 0, 0.4, 0.4 )
+  love.graphics.print(self.code_short4, 240, 130, 0, 0.4, 0.4 )
+  love.graphics.print(self.code_short5, 300, 130, 0, 0.4, 0.4 )
 
   love.graphics.setColor(255, 255, 255)
   fonts.set( 'big' )
 
   -- menu
-  local x = window.width / 2 - self.splash:getWidth()/2
-  local y = 2*window.height / 3 - self.splash:getHeight()/2
-  love.graphics.draw(self.splash, x, y)
-  love.graphics.draw(self.arrow, x + 12, y + 23 + 12 * (self.menu:selected() - 1))
-  for n,option in ipairs(self.menu.options) do
-    love.graphics.print(app.i18n(option), x + 23, y + 12 * n - 2, 0, 0.5, 0.5)
-  end
+  local x = window.width / 2 
+  local y = 2*window.height / 3 
+
 	
   -- control instructions
-  love.graphics.print(self.text, x - 25, window.height - 52, 0, 0.5, 0.5)
+  love.graphics.print(self.text, x - 80, window.height - 52, 0, 0.5, 0.5)
 
 end
 
@@ -88,6 +129,11 @@ function state:leave()
   self.line_short = nil
   self.line_count = nil
   self.line_timer = nil
+
+  self.code = nil
+  self.code_short = nil
+  self.code_count = nil
+  self.code_timer = nil
 
   self.splash = nil
   self.arrow = nil
