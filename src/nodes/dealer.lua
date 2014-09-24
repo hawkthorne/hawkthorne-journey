06cfadb6a14e74f980a23890a226d11f0d80ac89
Vrachno <jimmy_trooper2000@yahoo.com>
2014-09-24 21:02:58 +0300

    more indentation

--- src/nodes/dealer.lua
+++ src/nodes/dealer.lua
@@ -20,47 +20,44 @@ function Dealer.new(node, collider)
     dealer.bb = collider:addRectangle(node.x, node.y, node.width, node.height)
     dealer.bb.node = dealer
     collider:setPassive(dealer.bb)
-	
+  
     return dealer
-	
+  
 end
 
 function Dealer:enter(dt)
-	
-	fonts.reset()
+  
+  fonts.reset()
 
-	--Dealer says "Let's play poker" after a few seconds when player enters the tavern.
-	self.dialog = Timer.add(math.random(3,4), 
-		function()	
-			poker = Dialog.new("Let's play {{yellow}}poker{{white}}", 
-			--He repeats it every few seconds, sound only
-			function() 
-				self.loop = Timer.addPeriodic(math.random(10,20),
-				function()
-					sfx = sound.playSfx("letsPlayPoker")
-				end, 
-				math.huge) 
-			end
-						)
-		sound.playSfx("letsPlayPoker")
-		
-		end
-	)
+  --Dealer says "Let's play poker" after a few seconds when player enters the tavern.
+  self.dialog = Timer.add(math.random(3,4), 
+    function()
+      poker = Dialog.new("Let's play {{yellow}}poker{{white}}", 
+      --He repeats it every few seconds, sound only
+      function() 
+        self.loop = Timer.addPeriodic(math.random(10,20),
+        function()
+          sfx = sound.playSfx("letsPlayPoker")
+        end, 
+        math.huge) 
+      end
+            )
+     sound.playSfx("letsPlayPoker")  
+   end
+  )
 
-	
 
 function Dealer:leave()
-	--The timers are canceled upon leaving so the dialog and sound don't occur outside the tavern.
+  --The timers are canceled upon leaving so the dialog and sound don't occur outside the tavern.
    Timer.cancel(self.dialog)
-	if self.loop ~= nil then
-		Timer.cancel(self.loop)
-	end
-		
+  if self.loop ~= nil then
+    Timer.cancel(self.loop)
+  end
+    
 end
-		
-		
+    
+    
 end
-	
     
 
 
@@ -72,37 +69,37 @@ end
 
 function Dealer:keypressed( button, player )
 
-    if button == 'INTERACT' then
-        player.freeze = true
-		
-		--Timers for "Let's play poker" cancel upon interaction with the dealer.
-		Timer.cancel(self.dialog)
+  if button == 'INTERACT' then
+    player.freeze = true
+  
+  --Timers for "Let's play poker" cancel upon interaction with the dealer.
+    Timer.cancel(self.dialog)
 
-				if self.loop ~= nil then
-					Timer.cancel(self.loop)
-				end
-
-        local message = {'Choose a card game to play'}
-        local options = {'Poker', 'Blackjack', 'Exit'}
-
-        if player.money == 0 then
-          message = {'You dont have enough money!','Come back again...'}
-          options = {'Exit'}
-        end
-
-        local callback = function(result) 
-            self.prompt = nil
-            player.freeze = false
-            if result == 'Poker' or result == 'Blackjack' then
-              local screenshot = love.graphics.newImage( love.graphics.newScreenshot() )
-              Gamestate.stack(result:lower() .. 'game', player, screenshot)
-            end
-        end
-
-        self.prompt = Prompt.new(message, callback, options)
-        -- Key has been handled, halt further processing
-        return true
+    if self.loop ~= nil then
+      Timer.cancel(self.loop)
     end
+
+    local message = {'Choose a card game to play'}
+    local options = {'Poker', 'Blackjack', 'Exit'}
+
+    if player.money == 0 then
+      message = {'You dont have enough money!','Come back again...'}
+      options = {'Exit'}
+    end
+
+    local callback = function(result) 
+      self.prompt = nil
+      player.freeze = false
+      if result == 'Poker' or result == 'Blackjack' then
+        local screenshot = love.graphics.newImage( love.graphics.newScreenshot() )
+        Gamestate.stack(result:lower() .. 'game', player, screenshot)
+      end
+    end
+
+    self.prompt = Prompt.new(message, callback, options)
+      -- Key has been handled, halt further processing
+    return true
+  end
 end
 
 return Dealer
