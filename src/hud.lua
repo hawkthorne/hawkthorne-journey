222e7266a42733f43927f5b2db458c08745a8dfa
edisonout <edisonouthawkthorne@gmail.com>
2014-10-05 10:03:45 +0100

    add effect icons

--- src/hud.lua
+++ src/hud.lua
@@ -26,10 +26,13 @@ function HUD.new(level)
   hud.money = love.graphics.newImage('images/hud/money.png')
   hud.health_full = love.graphics.newImage('images/hud/health_full.png')
   hud.health_empty = love.graphics.newImage('images/hud/health_empty.png')
-
-  hud.money:setFilter('nearest', 'nearest')
-  hud.health_full:setFilter('nearest', 'nearest')
-  hud.health_empty:setFilter('nearest', 'nearest')
+  
+  hud.invincible = love.graphics.newImage('images/hud/invincible.png')
+  hud.restricted = love.graphics.newImage('images/hud/restricted.png')
+  hud.punchDamage = love.graphics.newImage('images/hud/punchDamage.png')
+  hud.jumpDamage = love.graphics.newImage('images/hud/jumpDamage.png')
+  hud.jumpFactor = love.graphics.newImage('images/hud/jumpFactor.png')
+  hud.speedFactor = love.graphics.newImage('images/hud/speedFactor.png')
 
   hud.saving = false
 
@@ -87,22 +90,22 @@ function HUD:questBadge( player )
     love.graphics.setColor( 255, 255, 255, 255 )
 end
 
-function HUD:draw( player )
+function HUD:draw(player)
   if not window.dressing_visible then
     return
   end
 
-  fonts.set( 'small' )
+  fonts.set('small')
 
-  self.x, self.y = camera.x, camera.y
+  local x, y = camera.x, camera.y
   love.graphics.setColor( 255, 255, 255, 255 )
   
   -- CHARACTER
-  love.graphics.draw(self.sheet, self.character_quad, self.x + 17, self.y + 27)
+  love.graphics.draw(self.sheet, self.character_quad, x + 17, y + 27)
   
   -- MONEY
-  love.graphics.print(player.money, self.x + 450, self.y + 35)
-  love.graphics.draw(self.money, self.x + 482, self.y + 33)
+  love.graphics.print(player.money, x + 450, y + 35)
+  love.graphics.draw(self.money, x + 482, y + 33)
   
   -- HEALTH 
   local heartValue = player.max_health / 10
@@ -111,9 +114,9 @@ function HUD:draw( player )
   for i = 1,2 do
     for j = 1,5 do
       if tracker + heartValue <= player.health then
-        love.graphics.draw(self.health_full, self.x + 58 + 13*(j-1), self.y + 35 + 13*(i - 1))
+        love.graphics.draw(self.health_full, x + 58 + 13*(j-1), y + 35 + 13*(i - 1))
       elseif tracker < player.health then
-        love.graphics.draw(self.health_empty, self.x + 58 + 13*(j-1), self.y + 35 + 13*(i - 1))
+        love.graphics.draw(self.health_empty, x + 58 + 13*(j-1), y + 35 + 13*(i - 1))
       end
       tracker = tracker + heartValue
     end
@@ -122,21 +125,57 @@ function HUD:draw( player )
   -- WEAPONS
   local currentWeapon = player.inventory:currentWeapon()
   if currentWeapon and not player.doBasicAttack or (player.holdingAmmo and currentWeapon) then
-    currentWeapon:drawHud(self.x + 480, self.y + 52, true)
+    currentWeapon:drawHud(x + 480, y + 52, true)
   end
 
   --SAVING
   if self.saving then
-    self.savingAnimation:draw(savingImage, self.x + 120 + 5, self.y + 40)
+    self.savingAnimation:draw(savingImage, x + 120 + 5, y + 40)
   end
   
-  --POTION EFFECTS
-  -- if player.activeEffects then
-    -- love.graphics.setColor( 0, 0, 0, 255 )
-    -- for i,effect in ipairs(player.activeEffects) do
-      -- love.graphics.printf(effect, self.x + 20, self.y + 40 + (20 * i), 350, "left",0,0.5,0.5)
-    -- end
-  -- end
+  --ACTIVE POTION & CONSUMABLE EFFECTS
+  if player.activeEffects then
+    love.graphics.setColor( 0, 0, 0, 255 )
+    for i,effect in ipairs(player.activeEffects) do
+      love.graphics.printf(effect, x + 20, y + 40 + (20 * i), 350, "left")
+    end
+  end
+  
+  love.graphics.setColor( 255, 255, 255, 255 )
+  local iconX, iconY = x + 145, y + 38
+  local icons = 0
+  
+  if player.godmode or player.invulnerable then --TODO: don't show up when getting hurt
+    love.graphics.draw(self.invincible, iconX + 20*icons, iconY)
+    icons = icons + 1
+  end
+  
+  if player.jumpFactor  > 1 then
+    love.graphics.draw(self.jumpFactor, iconX + 20*icons, iconY)
+    icons = icons + 1
+  end
+  
+  if player.speedFactor  > 1 then
+    love.graphics.draw(self.speedFactor, iconX + 20*icons, iconY)
+    icons = icons + 1
+  end
+
+  if player.jumpFactor == 0 or player.speedFactor == 0 then
+    love.graphics.draw(self.restricted, iconX + 20*icons, iconY)
+    icons = icons + 1
+  end
+  
+  if player.punchDamage > 1 then
+    love.graphics.draw(self.punchDamage, iconX + 20*icons, iconY)
+    icons = icons + 1
+  end
+
+  if player.jumpDamage > 3 then
+    love.graphics.draw(self.jumpDamage, iconX + 20*icons, iconY)
+    icons = icons + 1
+  end
+  
+  --TODO: add slide damage
 
   --QUESTS
   -- if player.quest ~= nil then
--- src/playerEffects.lua
+++ src/playerEffects.lua
@@ -7,7 +7,7 @@ local VALID_EFFECTS = {
   ['heal']=1,['buff']=2,['zombie']=3,['money']=4,['hurt']=5,['alcohol']=6
 }
 
-local HUDMessage = function(message, player, duration)
+local hudImage = function(message, player, duration)
   local d = duration or 4
   if not player.activeEffects then
     player.activeEffects = {message}
@@ -33,7 +33,7 @@ function PlayerEffects.buff(player, buff)
   player[buff.attribute] = buff.value
   Timer.add(buff.duration, function()
     player[buff.attribute] = orig
-    HUDMessage(buff.endMessage, player)
+    hudImage(buff.endMessage, player)
   end)
   player:potionFlash(buff.duration, buff.color or {192,192,192,255})
   return buff.startMessage .. (buff.startMessageValue and buff.value or "")
@@ -64,10 +64,10 @@ function PlayerEffects.zombie(player)
   local punchDamage, jumpFactor, jumpDamage, slideDamage, speedFactor, costume = player:initEffectsReset()
 
   player:addEffectsTimer(Timer.add(66, function () --Resets damage boost and costume after one minute being active
-    HUDMessage("a chilling gust of AC makes you forget your hunger for brains", player, 10)
+    hudImage("a chilling gust of AC makes you forget your hunger for brains", player, 10)
     player.resetPlayerEffects()
   end))
-  HUDMessage("that taco meat tastes weird...", player)
+  hudImage("that taco meat tastes weird...", player)
   for i=1,2 do
     player:addEffectsTimer(Timer.add(2*i-1, function () -- Damage over time
       if player.health > 1 then player:hurt(15) end
@@ -77,7 +77,7 @@ function PlayerEffects.zombie(player)
     if love.filesystem.exists("images/characters/" .. player.character.name .. "/zombie.png") then
       player.character.costume = 'zombie'
     end
-    HUDMessage("holy crap, you are a zombie!", player, 10)
+    hudImage("holy crap, you are a zombie!", player, 10)
     player.jumpDamage = jumpDamage * 2
     player.punchDamage = punchDamage * 2
     player.slideDamage = slideDamage * 2
@@ -85,18 +85,18 @@ function PlayerEffects.zombie(player)
 end
 
 function PlayerEffects.dudEffect(item, player)
-  HUDMessage("that " .. item .. " got stale and lost its power", player, 10)
+  hudImage("that " .. item .. " got stale and lost its power", player, 10)
 end
 
 function PlayerEffects.alcohol(player)
   local punchDamage, jumpFactor, jumpDamage, slideDamage, speedFactor, costume = player:initEffectsReset()
 
   player:addEffectsTimer(Timer.add(40, function () --Resets everything
-    HUDMessage("Sobering up", player)
+    hudImage("Sobering up", player)
     player.resetPlayerEffects()
   end))
 
-  HUDMessage("I think you drank too much...", player)
+  hudImage("I think you drank too much...", player)
   player.jumpFactor = jumpFactor * (math.random(40, 80) / 100)
   player.punchDamage = punchDamage * (math.random(40, 80) / 100)
   player.speedFactor = speedFactor * (math.random(40, 80) / 100)
@@ -114,7 +114,7 @@ function PlayerEffects:doEffect(effects, player)
     if effect == "randEffect" then
       self:randEffect(player, value)
     elseif VALID_EFFECTS[effect] then
-      HUDMessage(self[effect](player, value), player)
+      hudImage(self[effect](player, value), player)
     else
       error("Invalid player effect type: " .. effect)
     end
