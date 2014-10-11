2fdb7da110dc4606bf5bca56a900344d10c3e6ea
edisonout <edisonouthawkthorne@gmail.com>
2014-10-11 12:53:02 +0100

    add dealer sounds

--- src/nodes/dealer.lua
+++ src/nodes/dealer.lua
@@ -1,7 +1,11 @@
+local sound = require 'vendor/TEsound'
+local Dialog = require 'dialog'
+local Timer = require 'vendor/timer'
 local Gamestate = require 'vendor/gamestate'
 local Prompt = require 'prompt'
 local fonts = require 'fonts'
 local Dealer = {}
+
 Dealer.__index = Dealer
 -- Nodes with 'isInteractive' are nodes which the player can interact with, but not pick up in any way
 Dealer.isInteractive = true
@@ -20,7 +24,24 @@ function Dealer.new(node, collider)
 end
 
 function Dealer:enter(dt)
-  fonts.reset()
+	fonts.reset()
+	--Dealer says "Let's play poker" after a few seconds when player enters the tavern.
+	self.dialog = Timer.add(3, function()
+    poker = Dialog.new("Let's play {{yellow}}poker{{white}}", 
+	  --He repeats it every few seconds, sound only
+	  function()
+      self.loop = Timer.addPeriodic(15,function() sfx = sound.playSfx("letsPlayPoker")end, math.huge) 
+	  end)
+		sound.playSfx("letsPlayPoker")		
+	end)
+end
+  
+function Dealer:leave()
+	--The timers are canceled upon leaving so the dialog and sound don't occur outside the tavern.
+  Timer.cancel(self.dialog)
+  if self.loop ~= nil then
+    Timer.cancel(self.loop)
+  end
 end
 
 function Dealer:update(dt)
@@ -30,9 +51,15 @@ function Dealer:draw()
 end
 
 function Dealer:keypressed( button, player )
-
   if button == 'INTERACT' then
     player.freeze = true
+
+    --Timers for "Let's play poker" cancel upon interaction with the dealer.
+    Timer.cancel(self.dialog)
+
+    if self.loop ~= nil then
+      Timer.cancel(self.loop)
+    end
 
     local message = {'Choose a card game to play'}
     local options = {'Poker', 'Blackjack', 'Exit'}
