4dfc11e8d8684139d3b86be5461fd800e0730b99
edisonout <edisonouthawkthorne@gmail.com>
2014-10-11 20:28:29 +0100

    tidy up duplicate code

--- src/welcome.lua
+++ src/welcome.lua
@@ -37,20 +37,14 @@ function state:enter(previous)
   self.line_count = 1
   self.line_timer = 0
   self.code = " 5465415151 \n\n 5465415151 \n\n".. " 5465415151 \n\n 1651094097 \n\n 9046515154 \n\n 5490490456 \n\n 1571754564 \n\n 8940186515 \n\n 6454040940 \n\n 1651321894 \n\n 8979046541 \n\n 1216547789 \n\n 4651561165 \n\n 1121894897 \n\n 2278578589 \n\n 2857868678 \n\n 8678668287 \n\n 2784785786 \n\n 7868767867 \n\n 8678678678 \n\n 6786786786 \n\n 7867867867 \n\n 9447944794 \n\n 4794479447"
-  self.code_short = ""
-  self.code_short2 = ""
-  self.code_short3 = ""
-  self.code_short4 = ""
-  self.code_short5 = ""
-  self.code_short6 = ""
-  self.code_short7 = ""
-  self.code_count = 1
-  self.code_count2 = 1
-  self.code_count3 = 1
-  self.code_count4 = 1
-  self.code_count5 = 1
-  self.code_count6 = 1
-  self.code_count7 = 1
+  self.code_short = {}
+  self.code_count = {}
+  self.count_check = {}
+  for i = 1, 7 do
+    self.code_short[i] = ""
+    self.code_count[i] = 1
+    self.count_check[i] = 23*(i-1)
+  end
   self.code_timer = 0
   self.code_loaded = false
 
@@ -78,36 +72,16 @@ function state:update(dt)
     if self.line_count >135 then
       if self.code_timer > 0.005 then
         self.code_timer = 0
-        self.code_short = self.code_short..self.code.sub(self.code, self.code_count, self.code_count)
-        self.code_count = self.code_count + 1
-        if self.code_count >23 then
-          self.code_short2 = self.code_short2..self.code.sub(self.code, self.code_count2, self.code_count2)
-          self.code_count2 = self.code_count2 + 1
-        end
-        if self.code_count >46 then
-          self.code_short3 = self.code_short3..self.code.sub(self.code, self.code_count3, self.code_count3)
-          self.code_count3 = self.code_count3 + 1
-        end
-        if self.code_count >69 then
-          self.code_short4 = self.code_short4..self.code.sub(self.code, self.code_count4, self.code_count4)
-          self.code_count4 = self.code_count4 + 1
-        end
-        if self.code_count >92 then
-          self.code_short5 = self.code_short5..self.code.sub(self.code, self.code_count5, self.code_count5)
-          self.code_count5 = self.code_count5 + 1
-        end
-        if self.code_count >125 then
-          self.code_short6 = self.code_short6..self.code.sub(self.code, self.code_count6, self.code_count6)
-          self.code_count6 = self.code_count6 + 1
-        end
-        if self.code_count >148 then
-          self.code_short7 = self.code_short7..self.code.sub(self.code, self.code_count7, self.code_count7)
-          self.code_count7 = self.code_count7 + 1
+        for i = 1, 7 do
+          if self.code_count[1] > self.count_check[i] then
+            self.code_short[i] = self.code_short[i]..self.code.sub(self.code, self.code_count[i], self.code_count[i])
+            self.code_count[i] = self.code_count[i] + 1
+          end
         end
       end
     end
    end
-   if self.code_count > 310 then
+   if self.code_count[1] > 310 then
     self.code_loaded = true
   end
 end
@@ -124,22 +98,14 @@ function state:draw()
   love.graphics.setColor( 48, 254, 31, 225 )
   if self.code_loaded == false then
     love.graphics.print(self.line_short, 50, 50, 0, 0.5, 0.5 )
-    love.graphics.print(self.code_short, 50, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code_short2, 110, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code_short3, 170, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code_short4, 230, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code_short5, 290, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code_short6, 350, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code_short7, 410, 130, 0, 0.4, 0.4 )
-  else 
+    for i = 1, 7 do
+      love.graphics.print(self.code_short[i], 60*i - 10, 130, 0, 0.4, 0.4)
+    end
+  else
     love.graphics.print(self.line, 50, 50, 0, 0.5, 0.5 )
-    love.graphics.print(self.code, 50, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code, 110, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code, 170, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code, 230, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code, 290, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code, 350, 130, 0, 0.4, 0.4 )
-    love.graphics.print(self.code, 410, 130, 0, 0.4, 0.4 )
+    for i = 1, 7 do
+      love.graphics.print(self.code, 60*i - 10, 130, 0, 0.4, 0.4)
+    end
   end
 
   love.graphics.setColor(255, 255, 255)
@@ -157,8 +123,6 @@ function state:draw()
     self.menu_shown = true
   end
 
-
-	
   -- control instructions
   love.graphics.print(self.text, x - 25, window.height - 52, 0, 0.5, 0.5)
 
@@ -172,21 +136,10 @@ function state:leave()
   self.line_timer = nil
 
   self.code = nil
-  self.code_short = nil
-  self.code_count = nil
   self.code_timer = nil
-  self.code_short2 = nil
-  self.code_count2 = nil
-  self.code_short3 = nil
-  self.code_count3 = nil
-  self.code_short4 = nil
-  self.code_count4 = nil
-  self.code_short5 = nil
-  self.code_count5 = nil
-  self.code_short6 = nil
-  self.code_count6 = nil
-  self.code_short7 = nil
-  self.code_count7 = nil
+  self.code_count = {}
+  self.code_short = {}
+  self.count_check = {}
 
   self.splash = nil
   self.arrow = nil
