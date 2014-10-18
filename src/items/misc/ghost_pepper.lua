7b95c322786dffde3f54c09194e23fbdb6c1973f
8bitgentleman <mainmoniker@googlemail.com>
2014-10-17 23:05:43 -0400

    fix text overflow

--- src/items/misc/ghost_pepper.lua
+++ src/items/misc/ghost_pepper.lua
@@ -5,7 +5,7 @@ return{
     subtype = "projectile",
     damage = "5-10",
     special_damage = 'ice= 2,fire= 2',
-  	info = 'a spell to summon the ghost of peppers past',
+  	info = 'Summons the ghost of peppers past',
     MAX_ITEMS = 5,
     quantity = 1,
     directory = 'scrolls/'
