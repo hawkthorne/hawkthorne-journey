67e2d827a9b60ffc347a46c4be8a0642bbf77571
edisonout <edisonouthawkthorne@gmail.com>
2014-10-21 19:24:10 +0100

    fix flipping splats

--- src/nodes/splat.lua
+++ src/nodes/splat.lua
@@ -27,10 +27,9 @@ end
 
 function Splat:add(x,y,width,height)
 
-  local index = math.random(3)
-  local flipX = math.random(2) == 1
-  local flipY = math.random(2) == 1
-
+  local index_x = math.random(6)
+  local index_y = math.random(2)
+  
   table.insert(self.splats, {
     position = {
       x = x,
@@ -38,29 +37,27 @@ function Splat:add(x,y,width,height)
     },
     width = width,
     height = height,
-    wallQuad = self:wallQuad(y, height, index, flipY),
-    ceilingQuad = self:ceilingQuad(y, height, index, flipY),
-    floorQuad = self:floorQuad(y, height, index, flipY),
-    flipX = flipX,
-    flipY = flipY,
+    wallQuad = self:wallQuad(y, height, index_x, index_y),
+    ceilingQuad = self:ceilingQuad(y, height, index_x, index_y),
+    floorQuad = self:floorQuad(y, height, index_x, index_y),
   } )
 
 end
 
-function Splat:wallQuad(y, height, index, flipY)
-  local y = (y + height / 2 ) - self.splattersize.height / 2 + ( flipY and self.splattersize.height or 0 )
+function Splat:wallQuad(y, height, index_x, index_y)
+  local y = (y + height / 2 ) - self.splattersize.height / 2
   if y + self.splattersize.height > self.ceiling.height then
     -- splat starts in ceiling
     if y < self.ceiling.height then
       return love.graphics.newQuad(
-        (index-1)*self.splattersize.width, self.ceiling.height - y,
+        (index_x-1)*self.splattersize.width, (index_y -1)*self.splattersize.height + self.ceiling.height - y,
         self.splattersize.width, height - self.ceiling.height + y,
         self.splatters:getDimensions()
       )
     -- splat starts on wall
     elseif y < self.floor.y then
       return love.graphics.newQuad(
-        (index-1)*self.splattersize.width, 0,
+        (index_x-1)*self.splattersize.width, (index_y -1)*self.splattersize.height,
         self.splattersize.width, math.min(self.splattersize.height, self.floor.y - y),
         self.splatters:getDimensions()
       )
@@ -68,32 +65,32 @@ function Splat:wallQuad(y, height, index, flipY)
   end
 end
 
-function Splat:ceilingQuad(y, height, index, flipY)
-  local y = (y + height / 2 ) - self.splattersize.height / 2 + ( flipY and self.splattersize.height or 0 )
+function Splat:ceilingQuad(y, height, index_x, index_y)
+  local y = (y + height / 2 ) - self.splattersize.height / 2
   -- splat starts in ceiling
   if y < self.ceiling.height then
     return love.graphics.newQuad(
-      (index-1)*self.splattersize.width, 0, 
+      (index_x-1)*self.splattersize.width, (index_y -1)*self.splattersize.height, 
       self.splattersize.width, math.min(self.splattersize.height, self.ceiling.height - y),
       self.splatters:getDimensions()
     )
   end
 end
 
-function Splat:floorQuad(y, height, index, flipY)
-local y = (y + height / 2 ) - self.splattersize.height / 2 + ( flipY and self.splattersize.height or 0 )
+function Splat:floorQuad(y, height, index_x, index_y)
+local y = (y + height / 2 ) - self.splattersize.height / 2
   if y + self.splattersize.height > self.floor.y then
     -- splatter starts on wall
     if y < self.floor.y then
       return love.graphics.newQuad(
-        (index-1)*self.splattersize.width, self.floor.y - y,
+        (index_x-1)*self.splattersize.width, (index_y -1)*self.splattersize.height + self.floor.y - y,
         self.splattersize.width, math.min(self.floor.height, self.splattersize.height + y - self.floor.y),
         self.splatters:getDimensions()
       )
     -- splatter starts on floor
     else
       return love.graphics.newQuad(
-        (index-1)*self.splattersize.width, 0,
+        (index_x-1)*self.splattersize.width, (index_y -1)*self.splattersize.height,
         self.splattersize.width, self.floor.height,
         self.splatters:getDimensions()
       )
@@ -103,22 +100,18 @@ end
 
 function Splat:draw()
 
-  -- possibly do a check for existence?
-
   love.graphics.setColor( 255, 255, 255, 255 )
 
   for _,s in pairs( self.splats ) do
 
-    local x = (s.position.x + s.width / 2 ) - self.splattersize.width / 2 + ( s.flipX and self.splattersize.width or 0 )
-    local y = (s.position.y + s.height / 2 ) - self.splattersize.height / 2 + ( s.flipY and self.splattersize.height or 0 )
-    local flipX = s.flipX and -1 or 1
-    local flipY = s.flipY and -1 or 1
+    local x = (s.position.x + s.width / 2 ) - self.splattersize.width / 2
+    local y = (s.position.y + s.height / 2 ) - self.splattersize.height / 2
 
     if s.wallQuad then
       if y < self.ceiling.height then
-        love.graphics.draw(self.splatters, s.wallQuad, x, self.ceiling.height, 0, flipX, flipY)
+        love.graphics.draw(self.splatters, s.wallQuad, x, self.ceiling.height)
       else
-        love.graphics.draw( self.splatters, s.wallQuad, x, y, 0, flipX, flipY)
+        love.graphics.draw( self.splatters, s.wallQuad, x, y)
       end
     end
 
@@ -126,17 +119,14 @@ function Splat:draw()
 
     if s.floorQuad then
       if y < self.floor.y then
-        love.graphics.draw(self.splatters, s.floorQuad, x, self.floor.y, 0, flipX, flipY, 
-          -s.width + ( flipY and 51 or 0 ), 0, -1, 0 )
+        love.graphics.draw(self.splatters, s.floorQuad, x, self.floor.y)
       else
-        love.graphics.draw( self.splatters, s.floorQuad, x, y, 0, flipX, flipY,
-          -s.width - ( s.flipX and 51 or 0 ), 0, -1, 0 )
+        love.graphics.draw( self.splatters, s.floorQuad, x, y)
       end
     end
 
     if s.ceilingQuad then
-      love.graphics.draw( self.splatters, s.ceilingQuad, x, y, 0, flipX, flipY,
-        s.width / 2 + ( s.flipX and 51 or 0 ), 0, 1, 0 )
+      love.graphics.draw( self.splatters, s.ceilingQuad, x, y)
     end
   end
 
