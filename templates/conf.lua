cebfa87696708d566d03d2b5ed8ce6c97d58e589
Brendon Walsh <brendonwalsh@niamu.com>
2015-05-30 15:04:45 -0400

    Controller module enabled for releases

--- templates/conf.lua
+++ templates/conf.lua
@@ -2,13 +2,13 @@ function love.conf(t)
     t.title             = "Journey to the Center of Hawkthorne v{{ version }}"
     t.url               = "http://projecthawkthorne.com"
     t.author            = "https://github.com/hawkthorne?tab=members"
-    t.version           = "0.9.0"
+    t.version           = "0.9.1"
     t.identity          = "hawkthorne_release"
     t.window.width      = 1056
     t.window.height     = 672
     t.window.fullscreen = false
     t.console           = false
     t.modules.physics   = false
-    t.modules.joystick  = false
+    t.modules.joystick  = true
     t.release           = true
 end
