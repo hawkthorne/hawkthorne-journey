537b3eadf232d150b29e8165d9f22f6767651314
bucketh3ad <scrippman@gmail.com>
2015-06-13 10:13:42 -0600

    Update version.py for major version

--- scripts/version.py
+++ scripts/version.py
@@ -24,8 +24,13 @@ def next_minor_version():
     x, y, z = current_version_tuple()
     return "{0}.{1}.0".format(x,int(y)+1) 
 
+def next_major_version():
+    x, y, z = current_version_tuple()
+    return "{0}.0.0".format(x+1)
 
 def next_version():
+    if is_major():
+        return next_major_version()
     if is_release():
         return next_minor_version()
     else:
@@ -58,6 +63,20 @@ def is_release():
         return False
 
     return 'release' in pulls[0].get('title', '').lower()
+
+def is_major():
+    pulls_url = "https://api.github.com/repos/hawkthorne/hawkthorne-journey/pulls"
+    resp = requests.get(pulls_url, params={'state': 'closed', 'base': 'release'})
+
+    if not resp.ok:
+        return False
+
+    pulls = resp.json()
+
+    if len(pulls) == 0:
+        return False
+
+    return 'majorversion' in pulls[0].get('title', '').lower()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
