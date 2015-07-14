4e73c64cdd93de005d5d6864fc67ffca38124c73
Caleb John <calebjohn4@gmail.com>
2015-07-14 00:28:32 -0600

    fix tilda icon not appearing in inventory

--- src/npcs/oldman.lua
+++ src/npcs/oldman.lua
@@ -1,4 +1,6 @@
 local Timer = require 'vendor/timer'
+local Quest = require 'quest'
+local quests = require 'npcs/quests/tildaquest'
 local Dialog = require 'dialog'
 
 return {
@@ -50,30 +52,9 @@ return {
           npc.menu:close(player)
         end)
       else
-        script = {
-          "Huh? You say the Acorn King plans on destroying this town? You are as crazy as those filthy, long-haired hippies living high up in the mountains.",
-          "Get out of here young man, my life is hard enough without you crazy hipies stirring trouble up!",
-          "Alright, fine, I suppose there's no harm in indulging you in your crazy hippie-talk. So you want to know how to defeat the Acorn?",
-          "In the {{orange}}abandoned mine{{white}} up in the mountains, is a local cult who are said to worship Cornelius and his creatures.",
-          "It's supposed to contain numerous secrets such as the map to the Acorn King's hideout and ways to defeat him.",
-          "Now I don't know if the rumors are true or not, but it's definitely worth checking out.",
-          "Alright, now get out of my sight, I've got better things to be doing than talking to you.",
-        }
-        Dialogue = Dialog.create(script)
-        Dialogue:open(function()
-          Dialog.finished = true
-          player.quest = 'To Slay An Acorn - Explore the Mines for a Map to the Acorn King'
-          player.freeze = false 
-          player.minesDoor = true
-        end)
-
-        npc.menu:close(player)
-
-        player.freeze = true
-        npc.prompt = nil
-        Timer.add(2, function()
-          npc.fixed = false
-        end)
+        player.quest = nil
+        Quest.removeQuestItem(player)
+        Quest:activate(npc, player, quests.explore_mines)
       end
     end,
   },
